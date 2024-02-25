6c22d6267c0f8d7f3d0b4fff88a1ae2413e7b82f
Tushar kumar <tusharnwd2003@gmail.com>
2024-02-25 11:09:27 +0530

    string

---
+++ Day_24_to_30/StringInJava.java
@@ -0,0 +1,10 @@
+class StringInJava{
+  public static void main(String[] args) {
+    String name = "Tushar";
+    int Marks = 4855656;
+    System.out.printf("Hello %s how are you",name);
+    System.out.printf("Hello %s your Marks is %d",name,Marks);
+    System.out.printf(" Marks is %,d",Marks);
+
+  }
+}
