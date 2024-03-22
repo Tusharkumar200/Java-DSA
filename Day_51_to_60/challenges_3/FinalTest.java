4f164f291f7b90bdb78a43afaa4d344839a4ee98
Tushar kumar <tusharnwd2003@gmail.com>
2024-03-23 00:47:38 +0530

    Java_finished

---
+++ Day_51_to_60/challenges_3/FinalTest.java
@@ -0,0 +1,16 @@
+package challenges_3;
+
+import java.util.List;
+
+
+public class FinalTest {
+  public static void main(String[] args) {
+    List <String> numbers = List.of("1","2","3","4","5","6");
+    numbers.stream()
+        .map(Integer::parseInt)        
+        .map(num -> Math.pow(num, 2))
+        .reduce(Double::sum)
+        .ifPresent(System.out::println);
+  
+  }
+}
