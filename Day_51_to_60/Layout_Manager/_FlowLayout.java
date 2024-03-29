6bc7a47b9d018697fb5ecd0c0784fefbf5e041eb
Tushar kumar <tusharnwd2003@gmail.com>
2024-03-29 23:45:08 +0530

    name changed

--- Day_51_to_60/Layout_Manager/_FlowLayout.java
+++ Day_51_to_60/Layout_Manager/_FlowLayout.java
@@ -1,12 +1,11 @@
 package Layout_Manager;
 
-import java.awt.LayoutManager;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.awt.FlowLayout;
 
 
-public class FlowLayout {
+public class _FlowLayout {
 
 //  Layout Manager = Defines the natural layout for components within a container.
 // FlowLayout = places components in a row, sized at their preferred size.
@@ -18,8 +17,15 @@ public class FlowLayout {
   JFrame frame = new JFrame();
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   frame.setSize(500,500);
-  frame.setLayout((LayoutManager) new FlowLayout());
+  frame.setLayout(new _FlowLayout());
+  
+
   frame.add(new JButton("1"));
+  frame.add(new JButton("2"));
+  frame.add(new JButton("3"));
+  frame.add(new JButton("4"));
+  frame.add(new JButton("5"));
+  frame.add(new JButton("6"));
   frame.setVisible(true);
   }
 }
