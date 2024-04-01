f84607c6c2d2368af99d6026d5a3b9471d365371
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-01 23:49:42 +0530

    linearSearch

--- Day_61_to_70/LinerSearch.java
+++ Day_61_to_70/LinerSearch.java
@@ -1,5 +1,17 @@
 public class LinerSearch {
+
+  public static int search(int numbers[] , int key){
+    for (int i : numbers) {
+      if(i == key){
+        System.out.println("number found at "+ i);
+      }
+    }
+    return 0;
+  }
   public static void main(String[] args) {
-    
+    int[] numbers = {1,5,8,4,10,55,3,4,54,22};
+    int key = 10;
+
+    search(numbers, key);
   }
 }
