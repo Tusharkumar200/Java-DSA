fd1fca1451b1a44af2a72da5870f4215c95daa56
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-03 21:58:32 +0530

    formula added

--- Day_61_to_70/Pair_In_Array.java
+++ Day_61_to_70/Pair_In_Array.java
@@ -2,7 +2,9 @@ public class Pair_In_Array {
 
   public static void pair(int number[]){
 
-    int totalPairs = 0;
+    // int totalPairs = 0;
+    int totalPairs = number.length;
+
     for(int i=0; i<number.length;i++){
       int current = number[i];
       for(int j= i+1; j<number.length;j++){
@@ -12,7 +14,8 @@ public class Pair_In_Array {
       System.out.println();
       
     }
-    System.out.println("total pairs = "+totalPairs);
+    // System.out.println("total pairs = "+ totalPairs);
+    System.out.println("total pairs = "+ totalPairs*(totalPairs-1)/2);
 
   }
   public static void main(String[] args) {
