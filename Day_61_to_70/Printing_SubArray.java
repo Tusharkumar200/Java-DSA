c65d97c055c56bf18b021a4cc9f0160f2d1b9ecf
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-04 15:42:21 +0530

    added sum of all subarray

--- Day_61_to_70/Printing_SubArray.java
+++ Day_61_to_70/Printing_SubArray.java
@@ -2,12 +2,14 @@ public class Printing_SubArray {
 
   public static void subarray(int number[]){
     int ts = 0;
+    int sum=0;
     for (int i = 0; i < number.length; i++) {
       int start = i;
       for (int j = i; j < number.length; j++) {
         int end = j;
         for (int k = start; k <=end; k++) {
           System.out.print(number[k]+" ");
+          sum = sum+number[k];
         }
         ts++;
         System.out.println();
@@ -15,6 +17,7 @@ public class Printing_SubArray {
       System.out.println();
     }
     System.out.println("total subarray is: "+ts);
+    System.out.println("sum of total subarray is: "+sum);
   }
   public static void main(String[] args) {
     int number[] = {2,4,6,8,10};
