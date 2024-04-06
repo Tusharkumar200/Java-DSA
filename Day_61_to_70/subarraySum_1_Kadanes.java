86c0445da105b32964509e43998860cb62c9b0aa
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-06 23:46:33 +0530

    kadanes

--- Day_61_to_70/subarraySum_1_Kadanes.java
+++ Day_61_to_70/subarraySum_1_Kadanes.java
@@ -1,35 +1,7 @@
 public class subarraySum_1_Kadanes {
+  // !important
   // Kadane's approach 
-  public static void maxSubarraySum(int number[]){
-
-    // Time - Complexity --> O(n^2)
-    
-    int currSum=0;
-    int maxSum=Integer.MIN_VALUE;
-    int prefix[] = new int[number.length];
-    
-    prefix[0] = number[0];
-    // calculate prefix array
-    for (int i = 1; i < prefix.length; i++) {
-        prefix[i] = prefix[i-1] + number[i];
-
-    }
-
-    for (int i = 0; i < number.length; i++) {
-      int start = i;
-      for (int j = i; j < number.length; j++) {
-        int end = j;
-        currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start -1];
-
-        System.out.println(currSum);
-        if(maxSum < currSum){
-            maxSum = currSum;
-        }
-      }
-    }
-      System.out.println("max sum = "+ maxSum);
-    }
-        
+          // Time complexity --> O(n)  
     public static void kadanes(int number[]) {
       int ms =0;
       int cs =0;
