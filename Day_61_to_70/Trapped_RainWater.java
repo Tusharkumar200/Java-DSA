edbe3a8739807628ecc5e1d55b0e21285aee1428
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-07 17:58:10 +0530

    Trapped_RaingWater

---
+++ Day_61_to_70/Trapped_RainWater.java
@@ -0,0 +1,9 @@
+class Trapped_RaingWater{
+
+  public static int TrappedRaingWater(int height[]){
+    // calculate left max border
+    return 0;
+  }
+  public static void main(String[] args){
+    int height[] = {4,2,0,6,3,2,5};
+    TrappedRaingWater(hei
