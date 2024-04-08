48251f4d0bb9e55a54ed0286ea3a2ec55727b83c
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-08 22:03:06 +0530

    buyAndSellStocks

---
+++ Day_61_to_70/buyAndSellStocks.java
@@ -0,0 +1,9 @@
+public class buyAndSellStocks {
+  
+  public static int stocks(int prices[]){
+      return 0;
+  }
+  public static void main(String[] args) {
+    int prices[] = {7,1,5,3,6,4};
+  }
+}
