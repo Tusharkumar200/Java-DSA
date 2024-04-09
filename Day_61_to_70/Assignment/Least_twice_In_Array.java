4d9b06e0706f92f564ebd9d2c0f3df29ebf8b685
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-09 14:16:44 +0530

    ques01_approch_1_done

--- Day_61_to_70/Assignment/Least_twice_In_Array.java
+++ Day_61_to_70/Assignment/Least_twice_In_Array.java
@@ -11,7 +11,25 @@ Output: false
 */
 
 class Least_twice_In_Array{
+
+  public static boolean testing_Twice(int num[]){
+    boolean flag = false;
+    for (int i=0; i<num.length -1;i++) {
+      for (int j =i+1; j < num.length-1; j++) {
+        if(num[i] == num[j]){
+          flag = true;
+        }
+      }
+
+    }
+    return flag;
+
+  }
   public static void main(String [] args){
-    System.out.println("testing...");
+    int num[] = {1,2,3,4,5,1,1};
+        
+    boolean r= testing_Twice(num);
+    System.out.println(r);
+
   }
 }
