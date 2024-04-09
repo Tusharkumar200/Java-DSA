ba1ae2546d397e29ccac947ce7029e394f240ccd
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-09 13:29:13 +0530

    Least_twice_In_Array

---
+++ Day_61_to_70/Assignment/Least_twice_In_Array.java
@@ -0,0 +1,17 @@
+/*
+Question 1: Given an integer array nums, return true if any value appears at least twice in the 
+array, and return false if every element is distinct. 
+Example 1:
+Input: nums = [1, 2, 3, 1]
+Output: true
+Example 2:
+Input: nums = [1, 2, 3, 4]
+Output: false
+
+*/
+
+class Least_twice_In_Array{
+  public static void main(String [] args){
+    System.out.println("testing...");
+  }
+}
