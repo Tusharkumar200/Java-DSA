0dccef792b3ef07cbec86c825102b65ba1665b80
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-11 11:14:16 +0530

    prices listed

--- Day_61_to_70/Assignment/maximum_profit.java
+++ Day_61_to_70/Assignment/maximum_profit.java
@@ -1,7 +1,10 @@
 public class maximum_profit {
   
-  
+  public static void stock(int[] prices){
+    
+  }
   public static void main(String[] args) {
-        
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    stock(prices);
   }
 }
