6e2d7d7b515db6942996604eb3b8356a90587527
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-11 11:24:42 +0530

    function written

--- Day_61_to_70/Assignment/maximum_profit.java
+++ Day_61_to_70/Assignment/maximum_profit.java
@@ -1,7 +1,20 @@
 public class maximum_profit {
   
-  public static void stock(int[] prices){
-    
+  public static int stock(int[] prices){
+   int buyPrice = Integer.MAX_VALUE;
+   int maxProfit = 0;
+   
+   for (int i = 0; i < prices.length; i++) {
+      if(buyPrice < prices[i]){
+        int Profit = prices[i];
+        maxProfit = Math.max(maxProfit,Profit);
+
+      }
+      else{
+        buyPrice = prices[i];
+      }
+   }
+   return maxProfit;
   }
   public static void main(String[] args) {
     int prices[] = {7, 1, 5, 3, 6, 4};
