811d9aabf2d63991f3a37f708f39b4da19c455a4
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-12 23:34:08 +0530

    Testing

--- Day_61_to_70/Assignment/Trap_RainWater.java
+++ Day_61_to_70/Assignment/Trap_RainWater.java
@@ -13,12 +13,13 @@ public class Trap_RainWater{
       else{
         r--;
         rMax = Math.max(rMax, height[r]);
-        res = += rMax - height[r];
+        res  += rMax - height[r];
       }
     }
     return res;
   }
   public static void main(String[] args) {
     int []height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    trap(height);
   }
 }
