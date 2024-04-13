bfdd23d2d9ddba4442d3f7971c3dc2157c34a76e
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-13 15:09:15 +0530

    duplicate_triplets

---
+++ Day_61_to_70/Assignment/duplicate_triplets.java
@@ -0,0 +1,10 @@
+public class duplicate_triplets {
+  
+
+  public static void main(String[] args) {
+    int []nums = {-1, 0, 1, 2, -1, -4};
+    int result = duplicat(nums);
+    System.out.println(result);
+
+  }
+}
