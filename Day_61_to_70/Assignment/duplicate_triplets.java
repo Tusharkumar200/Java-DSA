25a0e1c189ebe6de28d99c362418dbb7b922a750
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-13 15:20:22 +0530

    TryingToSolve

--- Day_61_to_70/Assignment/duplicate_triplets.java
+++ Day_61_to_70/Assignment/duplicate_triplets.java
@@ -1,9 +1,21 @@
 public class duplicate_triplets {
   
-
+  public static int duplicate(int[] nums){
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums.length; j++) {
+        for (int k = 0; k < nums.length; k++) {
+          
+          if(i != j && i != k && j != k && nums[i] + nums[j] + nums[k] == 0){
+            mums[j] nums[k]]
+          }
+        }
+      }
+    }
+    return 0;
+  }
   public static void main(String[] args) {
     int []nums = {-1, 0, 1, 2, -1, -4};
-    int result = duplicat(nums);
+    int result = duplicate(nums);
     System.out.println(result);
 
   }
