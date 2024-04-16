2adaadff2186176fa25459ba4d0c4589efa0cc72
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-16 22:30:08 +0530

    updateed

--- Day_61_to_74/Pratice/print_duplicate_element.java
+++ Day_61_to_74/Pratice/print_duplicate_element.java
@@ -1,17 +1,24 @@
 public class print_duplicate_element{
 
-  public static int  duplicate(int[] nums){
-    for (int i=0 ;i<= nums.length;i++) {
-      if(nums[i] == nums[i] +1){
-        System.out.println(i);
+  public static void  duplicate(int[] nums){
+    
+    for (int i=0 ;i<= nums.length-1;i++) {
+      boolean isDuplicate = false;
+      for (int j = i+1; j < nums.length; j++) {
+         if(nums[i] == nums[j]){
+          isDuplicate = true; 
+          
+        }
+      }
+      if(isDuplicate){
+        System.out.println(nums[i]+" ");
       }
     }
-    return 0;
+    System.out.println();
   }
   public static void main(String[] args) {
     System.out.println("Duplicate in Array");
     int nums[] = {1,1,2,3,4,4,5,6,8,7,4};
-    int result = duplicate(nums);
-    System.out.println(result);
+    duplicate(nums);
   }
 }
