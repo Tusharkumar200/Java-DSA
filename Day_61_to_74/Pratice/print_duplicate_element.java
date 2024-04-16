c3c31dd5f80ac9657a94f7b586707a86b1a84d24
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-16 21:26:25 +0530

    print_duplicate_element

--- Day_61_to_74/Pratice/print_duplicate_element.java
+++ Day_61_to_74/Pratice/print_duplicate_element.java
@@ -8,6 +8,7 @@ public class print_duplicate_element{
   public static void main(String[] args) {
     System.out.println("Duplicate in Array");
     int nums[] = {1,1,2,3,4,4,5,6,8,7,4};
+    int result = duplicate(nums);
     
   }
 }
