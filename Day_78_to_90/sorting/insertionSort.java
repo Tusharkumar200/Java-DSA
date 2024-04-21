ff026f40887745f880acceac331b5d7416a91c2b
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-21 10:46:15 +0530

    instrtionSort

---
+++ Day_78_to_90/sorting/insertionSort.java
@@ -0,0 +1,17 @@
+public class insertionSort {
+  public static void insertion(int[] nums){
+    for (int i = 0; i < nums.length-1; i++) {
+      for (int j =i+1; j < nums.length-1; j++) {
+        
+      }
+    }
+  }
+  public static void printArray(int [] nums){
+    for (int i = 0; i < nums.length-1; i++) {
+      System.out.println(nums[i]);
+    }
+  }
+  int nums[] = {5,1,4,8,6,10,2};
+    Selection(nums);
+    printArray(nums);
+}
