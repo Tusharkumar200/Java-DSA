52f16606e63160f53101df92e714d7bf4262dd3a
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-21 11:01:38 +0530

    insertinsort

--- Day_78_to_90/sorting/insertionSort.java
+++ Day_78_to_90/sorting/insertionSort.java
@@ -1,9 +1,17 @@
 public class insertionSort {
   public static void insertion(int[] nums){
-    for (int i = 0; i < nums.length-1; i++) {
-      for (int j =i+1; j < nums.length-1; j++) {
-        
+    for (int i = 1; i < nums.length; i++) {
+      int curr=nums[i];
+      int pre = i-1;
+
+      // finding out the correct position to insert
+      while (pre >=0 && nums[pre] > curr) {
+        nums[pre+1] = nums[pre];
+        pre--;
       }
+      // instertion
+      nums[pre+1] =curr;
+
     }
   }
   public static void printArray(int [] nums){
@@ -11,7 +19,13 @@ public class insertionSort {
       System.out.println(nums[i]);
     }
   }
+
+  public static void main(String[] args) {
+    
+  
   int nums[] = {5,1,4,8,6,10,2};
-    Selection(nums);
-    printArray(nums);
+  insertion(nums);
+  printArray(nums);
+  }
 }
+
