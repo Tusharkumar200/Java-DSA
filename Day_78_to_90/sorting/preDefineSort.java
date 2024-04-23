32ec22e18a8ce66ca237a33bb8e602935e425959
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-23 19:05:18 +0530

    preDefineSort

--- Day_78_to_90/sorting/preDefineSort.java
+++ Day_78_to_90/sorting/preDefineSort.java
@@ -1,4 +1,24 @@
+import java.util.*;
+
 public class preDefineSort{
+
+  public static void preDef(int nums[]){
+    for (int i = 0; i < nums.length-1; i++) {
+      for (int j = 0; j < nums.length-1-i; j++) {
+          if(nums[j] > nums[j+1]){
+            int temp = nums[j+1];
+            nums[j+1] = nums[j];
+            nums[j]=temp;
+          }
+      }
+    }
+  }
+
+  public static void printArray(int[] arr){
+    for (int index = 0; index < arr.length; index++) {
+        System.out.println(" "+ index);
+    }
+  }
   public static void main(String[] args) {
     System.out.println("preDefineSort algo");
   }
