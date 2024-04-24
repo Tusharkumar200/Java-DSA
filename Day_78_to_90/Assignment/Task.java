3e5442af91efd8d623d2f6b5932c13ca8da8621f
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-25 01:00:30 +0530

    bubbleSort

--- Day_78_to_90/Assignment/Task.java
+++ Day_78_to_90/Assignment/Task.java
@@ -4,7 +4,7 @@ public class Task {
   public static void bubbleSort(int []nums){
       for (int i = 0; i < nums.length-1; i++) {
         for (int j = 0; j < nums.length-1; j++) {
-            if(nums[j] > nums[j+1]){
+            if(nums[j] < nums[j+1]){
               int temp = nums[j];
               nums[j] = nums[j+1];
               nums[j+1] = temp;
