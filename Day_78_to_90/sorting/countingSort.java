8abc0ebd508cd48b21450fe70b4aceaeb87a0a48
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-24 11:20:55 +0530

    countingSort

--- Day_78_to_90/sorting/countingSort.java
+++ Day_78_to_90/sorting/countingSort.java
@@ -1,7 +1,15 @@
 public class countingSort {
 
   public static void counting(int[] nums){
-    
+    int largest = Integer.MIN_VALUE;
+    for (int i = 0; i < nums.length; i++) {
+        largest = Math.max(largest,nums[i]);
+    }
+
+    int count[] = new int[largest+1];
+    for (int i = 0; i < nums.length; i++) {
+      count[nums[i]]++;
+    }
   }
   public static void main(String[] args) {
     int [] nums={1,5,2,4,7,8,3};
