aa05d039f8b0e9d209057eae4516f39655b335c4
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-27 15:15:11 +0530

    print2DArray

--- Day_78_to_90/2D_Array/printing_2D_Array.java
+++ Day_78_to_90/2D_Array/printing_2D_Array.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 
 public class printing_2D_Array {
   public static void main(String[] args) {
-    
+    int matrix[][] = new int[3][3];
+    int n = matrix.length;
+    int m = matrix[0].length;
+
+    @SuppressWarnings("resource")
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the matrix ");
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <m; j++) {
+          matrix[i][j] = input.nextInt();
+      }
+    }
+
+    // OutPut
+
+    for (int i = 0; i <n; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.print(matrix[i][j]+" ");
+      }
+      System.out.println();
+    }
   }
 }
