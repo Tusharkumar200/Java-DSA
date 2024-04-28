5103928f3ede919ed937aa14a3381e98ddfa60b8
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-28 11:05:18 +0530

    Topprintspiral

--- Day_78_to_90/2D_Array/spiral.java
+++ Day_78_to_90/2D_Array/spiral.java
@@ -1,8 +1,19 @@
 public class spiral {
 
   public static void printspiral(int matrix[][]){
-    
-  }
+
+    int startRow = 0;
+    int startCol = 0;
+    int endRow = matrix.length-1;
+    int endCol = matrix[0].length-1;
+
+    while(startRow <=endRow && startCol <= endCold){
+      // Top
+      for (int j =startCol;j <=endCol; j++) {
+        System.out.println(matrix[startRow] [j]+" ");
+      }
+    }
+    }
   public static void main(String[] args) {
     int matrix[][] = {
       {1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
