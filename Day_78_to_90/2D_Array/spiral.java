29378b13d26d58c75611b451512d631d39aa7fcb
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-28 11:12:56 +0530

    condition

--- Day_78_to_90/2D_Array/spiral.java
+++ Day_78_to_90/2D_Array/spiral.java
@@ -12,6 +12,22 @@ public class spiral {
       for (int j =startCol;j <=endCol; j++) {
         System.out.println(matrix[startRow] [j]+" ");
       }
+
+      
+      // Rigth
+      for (int i=startRow+1;i<=endRow; i++) {
+        System.out.println(matrix[i] [endCol]+" ");
+      }
+    // bottom
+    for(int j=endCol-1; j>=startCol;j--){
+      System.out.println(matrix[endRow][j]+" ");
+    }
+
+    // left
+    for (int i = endRow-1; i > startRow+1; i--) {
+      System.out.println(matrix[i][startCol] +" ");
+    }
+
     }
     }
   public static void main(String[] args) {
