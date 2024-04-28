39d300bfeb300294cec8ffb48cf3e99fb0bc7cb0
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-28 11:23:08 +0530

    TestingDone

--- Day_78_to_90/2D_Array/spiral.java
+++ Day_78_to_90/2D_Array/spiral.java
@@ -7,7 +7,7 @@ public class spiral {
     int endRow = matrix.length-1;
     int endCol = matrix[0].length-1;
 
-    while(startRow <=endRow && startCol <= endCold){
+    while(startRow <=endRow && startCol <= endCol){
       // Top
       for (int j =startCol;j <=endCol; j++) {
         System.out.println(matrix[startRow] [j]+" ");
@@ -20,18 +20,30 @@ public class spiral {
       }
     // bottom
     for(int j=endCol-1; j>=startCol;j--){
+      if(startRow== endRow){
+        break;
+      }
       System.out.println(matrix[endRow][j]+" ");
     }
 
     // left
     for (int i = endRow-1; i > startRow+1; i--) {
+      if(startCol == endCol){
+        break;
+      }
       System.out.println(matrix[i][startCol] +" ");
     }
 
+        startRow ++;
+        startCol ++;
+        endRow --;
+        endCol--;
     }
+    System.out.println();
     }
   public static void main(String[] args) {
     int matrix[][] = {
       {1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+      printspiral(matrix);
   }
 }
