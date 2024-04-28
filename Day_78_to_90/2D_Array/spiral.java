f39fcb79b4855d956ab3a241ccc2594db4c6807c
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-28 10:53:38 +0530

    printspiral

--- Day_78_to_90/2D_Array/spiral.java
+++ Day_78_to_90/2D_Array/spiral.java
@@ -1,4 +1,8 @@
 public class spiral {
+
+  public static void printspiral(int matrix[][]){
+    
+  }
   public static void main(String[] args) {
     int matrix[][] = {
       {1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
