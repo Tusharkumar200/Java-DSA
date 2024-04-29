102dd8ead2e2593d29d79d38ebf0843db829362a
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-29 23:37:13 +0530

    largestElement

---
+++ Day_61_to_77/Pratice/largestElement.java
@@ -0,0 +1,18 @@
+class largestElement{
+
+  public int large(int[] a){
+    int max = Integer.MIN_VALUE;
+    for (int i : a) {
+      if(i>max){
+        max=i;
+      }
+    }
+    return max;
+  }
+  public static void main(String []args)
+   
+    int a[] = {1,5,7,9,4,56,15,35};
+    int result = large(a);
+    System.out.println(result);
+  
+}
