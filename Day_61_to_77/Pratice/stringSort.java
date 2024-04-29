0b745081acfbe6f92a9ae9bee1e7b8516f17c70a
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-29 23:55:09 +0530

    string sorting methond

--- Day_61_to_77/Pratice/stringSort.java
+++ Day_61_to_77/Pratice/stringSort.java
@@ -1,9 +1,17 @@
 public class stringSort {
   public static void main(String[] args) {
     String name[] = {"Madras","Delhi","Ahameadabad","Calcutta","Bombay"};
-
+    String temp;
     for (int i = 0; i < name.length; i++) {
-      String 
+      for (int j = i+1; j < name.length; j++) {
+        if(name[j].compareTo(name[i])<0){
+            temp = name[i];
+            name[i] = name[j];
+            name[j] = temp;
+
+        }
+      }
+      System.out.println(name[i]);
     }
   }
 }
