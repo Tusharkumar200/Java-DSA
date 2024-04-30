bd7c08eb78f6f240a9f569ed49d8b5a6522c8677
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-30 23:11:29 +0530

    Searching_in_2D_Array

--- Day_78_to_90/2D_Array/Searching_in_2D_Array.java
+++ Day_78_to_90/2D_Array/Searching_in_2D_Array.java
@@ -1,3 +1,17 @@
 public class Searching_in_2D_Array {
-  
+  public static void search(int[][] matrix,int key){
+
+  }
+  public static void main(String[] args) {
+    int matrix[][] = {
+      {10,20,30,40},
+      {15,25,35,45},
+      {27,29,37,48},
+      {32,33,39,50}
+    };
+
+    int key = 33;
+
+    search(matrix,key);
+  }
 }
