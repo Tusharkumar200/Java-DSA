cb2fb3c411eb3ca1218772af64d5d348e2352aff
Tushar kumar <tusharnwd2003@gmail.com>
2024-04-30 22:23:44 +0530

    diagonalSum

--- Day_78_to_90/2D_Array/diagonalSum.java
+++ Day_78_to_90/2D_Array/diagonalSum.java
@@ -1,4 +1,20 @@
 public class diagonalSum {
+  public static int  diagonal_Sum(int [][] matrix){
+    int sum = 0;
+    for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[0].length; j++) {
+          
+          if(i ==j){
+            sum=+ matrix[i][j];
+          }
+          else if(i+j == matrix.length -1){
+            sum +=matrix[i][j];
+          }
+        }
+
+    }
+    return sum;
+  }
   
   public static void main(String[] args) {
     int matrix[][] = {
@@ -6,5 +22,8 @@ public class diagonalSum {
       {5,6,7,8},
       {9,10,11,12},
       {13,14,15,16}};
+
+      int result = diagonal_Sum(matrix);
+      System.out.println("Result : "+result);
   }
 }
