d3c2e8ee49635a82fe176673e3a065b05a73f286
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-01 10:08:21 +0530

    printing_number

--- Day_78_to_90/2D_Array/Assignment/printing_number.java
+++ Day_78_to_90/2D_Array/Assignment/printing_number.java
@@ -1,14 +1,22 @@
 package Assignment;
 
 public class printing_number {
-  p
-  public static int printNumber(int[][] array,int key){
-      return 0;
+    public static int printNumber(int[][] array,int key){
+      int count =0;
+      for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[0].length; j++) {
+            if(key == array[i][j]){
+               count++;
+            }
+        }
+      }
+      return count;
   }
   public static void main(String[] args) {
-    int[][] array = {{4,7,8},{8,8,7}};   
+    int[][] array = {{4,7,8},
+                     {8,8,7}};   
     int key = 7;
     int result = printNumber(array,key); 
-    System.out.println(result);
+    System.out.println("count of 7 is: "+result);
   }
 }
