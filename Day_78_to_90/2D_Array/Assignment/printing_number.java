26687d0f22104b2d12f8f05c87ef7cd504ff442e
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-01 09:50:18 +0530

    printing_number

---
+++ Day_78_to_90/2D_Array/Assignment/printing_number.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+public class printing_number {
+  p
+  public static int printNumber(int[][] array,int key){
+      return 0;
+  }
+  public static void main(String[] args) {
+    int[][] array = {{4,7,8},{8,8,7}};   
+    int key = 7;
+    int result = printNumber(array,key); 
+    System.out.println(result);
+  }
+}
