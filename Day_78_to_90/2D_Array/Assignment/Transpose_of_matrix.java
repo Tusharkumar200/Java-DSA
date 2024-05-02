4f37ae27e3ce4cdc79e0ef49a73b5ec029733a65
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-02 15:30:26 +0530

    Transpose_of_matrix

--- Day_78_to_90/2D_Array/Assignment/Transpose_of_matrix.java
+++ Day_78_to_90/2D_Array/Assignment/Transpose_of_matrix.java
@@ -2,23 +2,24 @@ package Assignment;
 
 public class Transpose_of_matrix {
 
-  public static int transpose(int[][] matrix){
+  public static void transpose(int[][] matrix){
     
     for (int row = 0; row < matrix.length; row++) {
-      for (int col = 0; col < matrix[0].length-1; col++) {
-        row = row + col;
-        col = row - col;
-        row = row - col;
+      for (int col = 0; col < matrix.length; col++) {
+        // row = row + col;
+        // col = row - col;
+        // row = row - col;
 
-        System.out.print(" "+ matrix[row][col]);
+        System.out.print(" "+ matrix[col][row]);
       }
     }
-    return 0;
+    
   }
   public static void main(String[] args) {
     int[][] matrix = {{2,4,6},
                       {5,8,10}};
-    int result = transpose(matrix);
-    System.out.print (result);
+
+    transpose(matrix);
+    System.out.println();
   }
 }
