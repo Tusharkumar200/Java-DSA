9b221f7e78cc8c19dea78dd59bf2d2a5d5b1ae77
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-02 11:25:58 +0530

    sum_of_the_numbers

--- Day_78_to_90/2D_Array/Assignment/sum_of_the_numbers.java
+++ Day_78_to_90/2D_Array/Assignment/sum_of_the_numbers.java
@@ -1,3 +1,26 @@
-public class sum_of_the_numbers {
+package Assignment;
+
+class sum_of_the_numbers {
+
+  public static int sumOfRow(int[][] array){
+    
+    int sum=0;
+    for (int row = 0; row < array.length; row++) {
+      for (int col = 0; col < array[0].length; col++) {
+        if(row ==2){
+          sum += row;
+        }
+      }
+    }
+    return sum;
+  }
   
+  public static void main(String[] args) {
+    int array[][]= {{1,4,9},
+                    {11,4,3},
+                    {2,2,3} };
+
+    int result = sumOfRow(array);
+    System.out.println(result);
+  }
 }
