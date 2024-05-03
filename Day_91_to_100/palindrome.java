c64cad76065d9e81e9647c2f09f4498419e2ae51
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-03 15:56:32 +0530

    palindrome

---
+++ Day_91_to_100/palindrome.java
@@ -0,0 +1,29 @@
+public class palindrome {
+
+  // T.c => O(n)
+  public static boolean isPalindrom(String str){
+
+    int n = str.length();
+    for (int i = 0; i < n/2; i++) {
+      
+      if(str.charAt(i) != str.charAt(n-1-i)){
+        return false;
+      }
+          
+  }
+
+    return true;
+  }
+  public static void main(String[] args) {
+    String str = "racecar";
+    
+
+    if (isPalindrom(str) == true) {
+      System.out.println(" this is palindrome ");
+    }
+    else{
+      System.out.println("not ");
+    }
+    
+}
+}
