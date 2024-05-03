1e8ad5c435bb4a9b1fde1e83b4627c57c3f5dab7
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-03 14:44:08 +0530

    Starting_String

---
+++ Day_91_to_100/_01_Starting_String.java
@@ -0,0 +1,11 @@
+class _01_Starting_String{
+  public static void main(String[] args) {
+    char[] letters = {'a','b','c','d'};
+    String name ="Tushar";
+
+    for (int i = 0; i < letters.length; i++) {
+      System.out.println("letters : "+letters[i]);
+    }
+    System.out.println(name);
+  }
+}
