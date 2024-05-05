eae2e6989b617db3ab4d58fa539d95988dfed64b
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-05 22:22:52 +0530

    outputOfTheProgram

--- Day_91_to_100/Assignment/_03_outputOfTheProgram.java
+++ Day_91_to_100/Assignment/_03_outputOfTheProgram.java
@@ -1,5 +1,8 @@
 package Assignment;
 
 public class _03_outputOfTheProgram {
-  
+  public static void main(String args[]) {
+    String str = "ApnaCollege".replace("l", "");
+    System.out.println(str);
+    }
 }
