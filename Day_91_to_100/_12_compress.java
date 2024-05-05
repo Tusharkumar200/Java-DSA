f1223f34e61af1bfa4b50fdf73584e2416d95dc9
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-05 11:31:29 +0530

    compress

---
+++ Day_91_to_100/_12_compress.java
@@ -0,0 +1,14 @@
+public class _12_compress {
+  // example --> aaabbc -->a3b2c;
+
+  public static String compress(String str){
+    String newStr = "";
+    StringBuilder sb = new StringBuilder("");
+    
+    return newStr.toString();
+  }
+  public static void main(String[] args) {
+    String str = "aaabbbcc";
+    System.out.println(compress(str));
+  }
+}
