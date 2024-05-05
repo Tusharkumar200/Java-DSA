7e05c980c750fa5f82147fc4fb27a46e3d3d4902
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-05 11:42:05 +0530

    compress

--- Day_91_to_100/_12_compress.java
+++ Day_91_to_100/_12_compress.java
@@ -3,9 +3,20 @@ public class _12_compress {
 
   public static String compress(String str){
     String newStr = "";
-    StringBuilder sb = new StringBuilder("");
-    
-    return newStr.toString();
+   
+    for (int i = 0; i < str.length(); i++) {
+      Integer count =1;
+      while (i<str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
+        count++;
+        i++;
+      }
+      newStr +=str.charAt(i);
+      if(count > 1){
+        newStr += count.toString();
+      }
+    }
+
+    return newStr;
   }
   public static void main(String[] args) {
     String str = "aaabbbcc";
