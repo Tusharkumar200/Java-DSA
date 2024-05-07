716f8e68fd7f6e7078b7e19bd7615952cc7cd961
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-07 15:29:41 +0530

    _349_intersectionOfTwoArray

--- Day_91_to_100/Array_Leetcode_problem/_349_intersectionOfTwoArray.java
+++ Day_91_to_100/Array_Leetcode_problem/_349_intersectionOfTwoArray.java
@@ -1,7 +1,43 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class _349_intersectionOfTwoArray {
+
+  public static void intersection(int[] num1, int[] num2){
+
+        Arrays.sort(num1);
+        Arrays.sort(num2);
+        int i=0;
+        int j=0;
+        Set<Integer> set = new HashSet<Integer>(); 
+        
+        while(i < num1.length && j < num2.length){
+          if(num1[i] == num2[j]){
+            set.add(num1[i]);
+            i++;
+            j++;
+          }
+          else if(num1[i] < num2[j]){
+            i++;
+           
+          }
+          else j++;
+        }
+
+        int[] result = new int[set.size()];
+        int index = 0;
+        for(var num:set) {
+          result[index] = Integer.parseInt(num.toString());
+          index++;
+          System.out.println( result );
+        }
+  }
   public static void main(String[] args) {
-    int num1[] ={1,2,2,1};
-    int num2[] = {2,2};
+    int num1[] ={1,2,2,1,9};
+    int num2[] = {2,2,9};
+    intersection(num1,num2);
     
+
   }
 }
--- Day_91_to_100/Assignment/_04_anagramsString.java
+++ Day_91_to_100/Assignment/_04_anagramsString.java
@@ -1,35 +1,37 @@
 package Assignment;
+
 import java.util.Arrays;
+
 public class _04_anagramsString {
 
-  public static void anagrams(String str1,String str2){
+  public static void anagrams(String str1, String str2) {
     str1 = str1.toLowerCase();
     str2 = str2.toLowerCase();
 
-    if(str1.length() == str2.length()){
+    if (str1.length() == str2.length()) {
       char[] str1charArray = str1.toCharArray();
       char[] str2charArray = str2.toCharArray();
 
       Arrays.sort(str1charArray);
       Arrays.sort(str2charArray);
 
-      boolean result = Arrays.equals(str1charArray,str2charArray);
+      boolean result = Arrays.equals(str1charArray, str2charArray);
 
-      if(result) {
+      if (result) {
         System.out.println(str1 + " and " + str2 + " are anagrams of each other.");
-      }
-      else {
+      } else {
         System.out.println(str1 + " and " + str2 + " are not anagrams of each other.");
       }
-  }
-  else {
-    // case when lengths are not equal
-    System.out.println(str1 + " and " + str2 + " are not anagrams of each other.");
+    } else {
+      // case when lengths are not equal
+      System.out.println(str1 + " and " + str2 + " are not anagrams of each other.");
     }
-  public static void main(String[] args) {
-    String str1 ="earth";
-    String str2 ="heart";
+  }
 
-    anagrams(str1,str2);
+  public static void main(String[] args) {
+    String str1 = "earth";
+    String str2 = "heart";
+
+    anagrams(str1, str2);
   }
 }
