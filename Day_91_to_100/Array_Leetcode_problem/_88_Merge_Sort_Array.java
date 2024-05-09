10246ef93a20f07c2a1924b2e37e6d917c6277de
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-09 15:09:04 +0530

    fastMerge

--- Day_91_to_100/Array_Leetcode_problem/_88_Merge_Sort_Array.java
+++ Day_91_to_100/Array_Leetcode_problem/_88_Merge_Sort_Array.java
@@ -12,12 +12,48 @@ public class _88_Merge_Sort_Array {
        
 
   }
+  
+  public static int[] fastMerge(int[] nums1, int m,int[] nums2, int n){
+        int i=0;
+        int j=0;
+        int[] arr = new int[nums1.length];
+        int index =0; // arr 
+        while(i < m && j < n){
+            if(nums1[i] <= nums2[j]){
+                arr[index] = nums1[i];
+                index++;
+                i++;
+            }
+            else{
+              arr[index] = nums2[j];
+              index++;
+              j++;
+
+            }
+        }
+        while(i < m){
+          arr[index]=nums1[i];
+          index++;
+          i++;
+        }
+        while(j < n){
+          arr[index]=nums2[j];
+          index++;
+          j++;
+        }
+        return arr;
+  }
   public static void main(String[] args) {
     int m=3;
     int n=3;
     int num1[]={1,2,3,0,0,0};
     int num2[] = {2,5,6};
-    merge(num1,m, num2,n);
-    System.out.println(Arrays.toString(num1));
-  }
+
+    var st = System.nanoTime();
+    // merge(num1,m, num2,n);
+    num1 =fastMerge(num1,m, num2,n);
+    var et = System.nanoTime();
+    System.out.println(Arrays.toString(num1 )+"Time Taken :"+(et+st));
+    
+  };
 }
