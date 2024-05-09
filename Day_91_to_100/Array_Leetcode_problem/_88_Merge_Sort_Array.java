2b484dc36566249ebe72ab71506d168aa4e423ea
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-09 14:35:16 +0530

    merge

--- Day_91_to_100/Array_Leetcode_problem/_88_Merge_Sort_Array.java
+++ Day_91_to_100/Array_Leetcode_problem/_88_Merge_Sort_Array.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
+
 public class _88_Merge_Sort_Array {
 
   public static void merge(int[] num1, int m,int[] num2, int n){
+      int index =0;
+      for(int i=m; i<num1.length;i++){
+        num1[i] = num2[index];
+        index++;
+      }
+       Arrays.sort(num1);
+       
 
   }
   public static void main(String[] args) {
