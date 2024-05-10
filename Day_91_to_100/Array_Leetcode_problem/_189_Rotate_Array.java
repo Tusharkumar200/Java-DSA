037c367ecd994110663fa52ff512d18fee2e7b17
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-10 14:04:02 +0530

    _189_Rotate_Array

--- Day_91_to_100/Array_Leetcode_problem/_189_Rotate_Array.java
+++ Day_91_to_100/Array_Leetcode_problem/_189_Rotate_Array.java
@@ -1,22 +1,27 @@
+import java.util.Arrays;
+
 public class _189_Rotate_Array {
 
   public static void rotate(int nums[],int k){
-    int index =0;
+    
     int len = nums.length;
-    int newPos=0;
-    for(int i=0;i<len;i++){
-       newPos = (nums[index]+k)%len;
-       index++;
+    int [] result = new int[len];
 
-      
-       System.out.print(" "+newPos);
+    for (int index = 0; index < nums.length; index++) {
+        result[(index +k) % len] = nums[index];
     }
-  }
+    
+    for (int i = 0; i <len; i++) {
+      nums[i] = result[i];
+
+    }
+    
+    }
 
   public static void main(String[] args) {
     int[] nums= {1,2,3,4,5,6,7};
-    int k=3;
+    int k=5;
     rotate(nums, k);
-    System.out.println(nums);
+    System.out.println(Arrays.toString(nums));
   }
 }
