9c85f011144ecc175028d9f2a9a03f96d600d73d
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-11 14:20:28 +0530

    Testing

---
+++ Day_91_to_100/LinkedList_DS/Testing.java
@@ -0,0 +1,18 @@
+package LinkedList_DS;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Testing{
+  public static void main(String[] args) {
+    List<Integer> list = new LinkedList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    System.out.println(list);
+  }
+}
+
+
