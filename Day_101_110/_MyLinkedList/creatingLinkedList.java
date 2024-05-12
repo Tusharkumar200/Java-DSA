71d7f5bcac9d8515cf73107b4fa3f63e2a700989
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-12 13:03:23 +0530

    insert_In_linkedList

--- Day_101_110/_MyLinkedList/creatingLinkedList.java
+++ Day_101_110/_MyLinkedList/creatingLinkedList.java
@@ -1,7 +1,37 @@
 package _MyLinkedList;
 
+class Node{
+  public int data;
+  public Node next = null;
 
+  public Node(int data){
+    this.data = data;
+  }
+
+}
 public class creatingLinkedList {
 
+  private Node head;
+
+  public creatingLinkedList(){
+    head = null;
+  }
+  
+  public void insert(int value){
+    Node node = new Node(value);
+
+    if(head == null){
+      head = node;
+    }
+    else{
+        Node current = head;
+
+        while(current.next != null){
+          current = current.next;
+        }
+        current.next = node;
+    }
+    
+  }
   
 }
