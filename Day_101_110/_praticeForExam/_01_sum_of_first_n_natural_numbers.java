45480b320a650c021b854c3ef3b4fd4d8bb27b7f
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-13 14:14:49 +0530

    average

--- Day_101_110/_praticeForExam/_01_sum_of_first_n_natural_numbers.java
+++ Day_101_110/_praticeForExam/_01_sum_of_first_n_natural_numbers.java
@@ -2,13 +2,14 @@ package _praticeForExam;
 
 public class _01_sum_of_first_n_natural_numbers {
 
-  public static void sumOFNumber(int num){
+  public static int sumOFNumber(int num){
     int sum =0;
     for (int i = 0; i <= num; i++) {
        System.out.println(i);
        sum += i;
     }
-    System.out.println("sum is: "+sum);
+    
+    return sum;
   }
   public static void main(String[] args) {
     sumOFNumber(6);
--- Day_101_110/_praticeForExam/_02_average.java
+++ Day_101_110/_praticeForExam/_02_average.java
@@ -1,5 +1,10 @@
 package _praticeForExam;
 
 public class _02_average {
-  
+  public static void main(String[] args) {
+    _01_sum_of_first_n_natural_numbers natural_numbers = new _01_sum_of_first_n_natural_numbers();
+    int num =5;
+    int avg = natural_numbers.sumOFNumber(num);
+    System.out.println("avg: "+avg/num);
+  }
 }
