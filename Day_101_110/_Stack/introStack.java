4de8830902aedc8652a768ed9bbe34c1ef053bb0
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-22 11:05:24 +0530

    introStack

--- Day_101_110/_Stack/introStack.java
+++ Day_101_110/_Stack/introStack.java
@@ -1,6 +1,16 @@
+import java.util.Stack;
+
 public class introStack {
   public static void main(String[] args) {
     Stack<Integer> stack = new Stack<>();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    System.out.println(stack);
+   
+
 
   }
 }
