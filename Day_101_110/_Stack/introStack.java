b94a3f32ba040c139a991e7865688aea6622136c
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-22 11:09:24 +0530

    push&pop

--- Day_101_110/_Stack/introStack.java
+++ Day_101_110/_Stack/introStack.java
@@ -3,13 +3,20 @@ import java.util.Stack;
 public class introStack {
   public static void main(String[] args) {
     Stack<Integer> stack = new Stack<>();
+    
+    // push
     stack.push(1);
     stack.push(2);
     stack.push(3);
     stack.push(4);
     stack.push(5);
     System.out.println(stack);
-   
+    
+    // pop
+    stack.pop();
+    stack.pop();
+
+    System.out.println(stack);
 
 
   }
