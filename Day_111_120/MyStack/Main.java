072b995a006f2a1c381f74956f9d049686fe55d8
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-23 15:59:27 +0530

    push_and_size

--- Day_111_120/MyStack/Main.java
+++ Day_111_120/MyStack/Main.java
@@ -3,5 +3,14 @@ package MyStack;
 public class Main {
   public static void main(String[] args) {
     MyStack stack = new MyStack(5);
+    stack.push(5);
+    stack.push(4);
+    stack.push(3);
+    stack.push(2);
+    stack.push(1);
+    
+
+    System.out.println("size : "+stack.size());
+    System.out.println(stack);
   }
 }
--- Day_111_120/MyStack/MyStack.java
+++ Day_111_120/MyStack/MyStack.java
@@ -6,9 +6,30 @@ public class MyStack {
   private int size;
 
   public MyStack(int size){
-     this.size;
+     this.size = size;
      this.items = new int[size];
      this.top = 0;
+  }
+
+  public void push(int value){
+    if(top == size){
+      System.out.println("Stack is full");
+      return;
+    }
+    this.items[this.top] = value;
+    this.top++;
+  }
+
+  public int size(){
+    return top;
+  }
+
+  public String toString(){
+    StringBuilder stringBuilder = new StringBuilder();
+    for(int i = size -1 ; i>=0;i--){
+      stringBuilder.append(items[i]).append(" \n");
+    }
+    return stringBuilder.toString();
   }
 
 }
