c492fb490083d7522f6427d91a83b67973f3fc40
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-23 15:36:57 +0530

    size

--- Day_111_120/MyStack/Main.java
+++ Day_111_120/MyStack/Main.java
@@ -1,5 +1,7 @@
 package MyStack;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    MyStack stack = new MyStack(5);
+  }
 }
--- Day_111_120/MyStack/MyStack.java
+++ Day_111_120/MyStack/MyStack.java
@@ -1,5 +1,14 @@
 package MyStack;
 
 public class MyStack {
-  
+  private int[] items;
+  private int top;
+  private int size;
+
+  public MyStack(int size){
+     this.size;
+     this.items = new int[size];
+     this.top = 0;
+  }
+
 }
