66529c8ac9e7353b44d0779c292b8ca5395f8efb
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-29 08:04:27 +0530

    pop

--- Day_111_120/LeetCode/TwoStack.java
+++ Day_111_120/LeetCode/TwoStack.java
@@ -5,8 +5,10 @@ public class TwoStack {
   private int[] items;
   private int top1;
   private int top2;
+  private int size;
 
   public TwoStack(int s){
+    this.size = s;
       this.items = new int[s];
       this.top1= 0;
       this.top2= s-1;
@@ -25,5 +27,16 @@ public class TwoStack {
     
     items[top2] = num;
     top2++;
+  }
+
+  public int pop1(){
+    if(top1 == 0) return -1;
+    top1--;
+    return items[top1];
+  }
+  public int pop2(){
+    if(top2 == size-1) return -1;
+    top2++;
+    return items[top2];
   }
 }
