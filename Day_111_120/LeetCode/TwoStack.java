7cceadda27edf313e7bbe7be81e7e05abaf6777e
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-29 07:58:08 +0530

    push

--- Day_111_120/LeetCode/TwoStack.java
+++ Day_111_120/LeetCode/TwoStack.java
@@ -2,15 +2,28 @@ package LeetCode;
 
 public class TwoStack {
   
-  
+  private int[] items;
+  private int top1;
+  private int top2;
+
   public TwoStack(int s){
+      this.items = new int[s];
+      this.top1= 0;
+      this.top2= s-1;
 
   }
 
   public void push1(int num){
+    if(top1 > top2) return;
     
+    items[top1] = num;
+    top1++;
+
   }
   public void push2(int num){
+    if(top1 > top2) return;
     
+    items[top2] = num;
+    top2++;
   }
 }
