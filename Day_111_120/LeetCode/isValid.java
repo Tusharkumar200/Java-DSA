1fe1aa4d7820adc9c521b15bdf8f19feb1b2d3bb
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-30 23:16:50 +0530

    isValidParenthesis

--- Day_111_120/LeetCode/isValid.java
+++ Day_111_120/LeetCode/isValid.java
@@ -1,7 +1,33 @@
 package LeetCode;
 
+import java.util.Stack;
+
 public class isValid {
   public static boolean isValidParenthesis(String expression){
-    return true;
+    Stack <Character> stack = new Stack<>();
+
+    for (char ch: expression.toCharArray()){
+      if(
+        ch == '{' ||
+        ch == '(' ||
+        ch == ']'){
+
+          // opening Bracket
+          stack.push(ch);
+
+        }
+        else{
+          if(stack.isEmpty()) return false;
+          char top = stack.pop();
+
+          if(
+            ch == '}'&& top != '{' ||
+            ch == ']'&& top != '[' ||
+            ch == ')'&& top != '('
+          ) return false;
+        }
+
+  }
+    return stack.isEmpty();
   }
 }
