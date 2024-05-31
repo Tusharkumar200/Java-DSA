825a0e16c1f0c3d3a210d59a951000e7999ff641
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-31 20:31:06 +0530

    element_at_bottom

---
+++ Day_111_120/LeetCode/element_at_bottom.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class element_at_bottom {
+  public static Stack<Integer> pushAtBottom(stack <Integer> myStack, int x){
+        Stack<Integer> temp = new Stack<>();
+
+        
+  }
+}
