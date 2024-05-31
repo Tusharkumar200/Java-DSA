f2126e47b75e2e303656f514e10c86dc16114ded
Tushar kumar <tusharnwd2003@gmail.com>
2024-05-31 20:38:14 +0530

    pushAtBottom

--- Day_111_120/LeetCode/element_at_bottom.java
+++ Day_111_120/LeetCode/element_at_bottom.java
@@ -3,9 +3,18 @@ package LeetCode;
 import java.util.Stack;
 
 public class element_at_bottom {
-  public static Stack<Integer> pushAtBottom(stack <Integer> myStack, int x){
+
+  public static Stack<Integer> pushAtBottom(Stack <Integer> myStack, int x){
         Stack<Integer> temp = new Stack<>();
 
+        while(!myStack.isEmpty())
+          temp.push(myStack.pop());
+        temp.push(x);
         
+
+        while(!temp.isEmpty()){
+          myStack.push(temp.pop());
+        }
+        return myStack;
   }
 }
