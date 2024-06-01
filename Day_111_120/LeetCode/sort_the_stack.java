006e60b8dfb660c2073524ace1d8f6033b4bad24
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-02 00:24:56 +0530

    sortStack

--- Day_111_120/LeetCode/sort_the_stack.java
+++ Day_111_120/LeetCode/sort_the_stack.java
@@ -6,7 +6,21 @@ public class sort_the_stack{
 
     public static void insertSorted(Stack<Integer> stack , int num){
 
-        
+        if(stack.isEmpty()){
+            stack.push(num);
+            return;
+        }
+
+        int top  = stack.peek();
+
+        if(top <= num){
+            stack.push(num);
+        }
+        else{
+            stack.pop();
+            insertSorted(stack,num);
+            stack.push(top);
+        }
     }
 
     public static void sortStack(Stack<Integer> stack){
