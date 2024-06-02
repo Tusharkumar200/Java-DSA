0763695fdbed25b7b060adbc8aa10f29f194fdcc
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-03 01:37:34 +0530

    Redundant_brackets

--- Day_111_120/LeetCode/Redundant_brackets.java
+++ Day_111_120/LeetCode/Redundant_brackets.java
@@ -1,5 +1,29 @@
+package LeetCode;
+
 import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+
 public class Redundant_brackets{
-    
+    public static boolean findRedundantBrackets(String s){
+        Stack<Character> stack = new Stack<>();
+        List<Character> operators =  Arrays.asList('+','-','*','/');
+
+        for(char ch: s.toCharArray()){
+
+            if(ch == '(' || operators.contains(ch)){
+                stack.push(ch);
+            }
+            else if(ch == ')'){
+                char top = stack.pop();
+                if(!operators.contains(top)) return true;
+
+                while(!stack.isEmpty() && operators.contains(stack.peek())){
+                    stack.pop();
+                }
+                stack.pop();
+            }
+        }
+        return false;
+        
+    }
 }
