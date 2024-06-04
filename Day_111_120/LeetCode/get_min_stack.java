f66f0440fe43a753bee0fa3001c9e80f10d5c166
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-04 13:47:48 +0530

    get_min_stack

--- Day_111_120/LeetCode/get_min_stack.java
+++ Day_111_120/LeetCode/get_min_stack.java
@@ -1,5 +1,33 @@
 import java.util.Stack;
-
+// O(n) time complexity
 public class get_min_stack {
-    
+    Stack<Integer> minStack;
+    Stack<Integer> stack;
+
+    public get_min_stack() {
+        this.minStack = new Stack<>();
+        this.stack = new Stack<>();
+    }
+
+    int getMin(){
+
+    }
+    int pop(){
+
+    }
+    void push(int x)
+    {
+        if(stack.isEmpty()){
+            stack.push(x);
+            minStack.push(x);
+            return;
+        }
+        int minTillNow = minStack.peek();
+        if(x < minTillNow){
+            minStack.push(x);
+        }else{
+            minStack.push(minTillNow);
+        }
+    }
+
 }
