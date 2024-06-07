49fff7613d2eb971b8dde9b0b0c0e819cb741e8e
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-07 09:50:27 +0530

    QueueUsingStack

---
+++ Day_122_to_130/myQueue/QueueUsingStack.java
@@ -0,0 +1,14 @@
+package myQueue;
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+
+    public QueueUsingStack() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+}
