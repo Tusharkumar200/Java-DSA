a5d92fa25e179cb470230bb95ddd711c873adcec
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-07 10:49:50 +0530

    public class testing_priorityQueue

---
+++ Day_122_to_130/myQueue/testing_priorityQueue.java
@@ -0,0 +1,15 @@
+package myQueue;
+
+public class testing_priorityQueue {
+
+    public static void main(String[] args) {
+        
+        myPriorityQueue pq = new myPriorityQueue(5);
+        pq.enqueue(1);
+        pq.enqueue(2);
+        pq.enqueue(4);
+        pq.enqueue(5);
+    
+        System.out.println(pq.dequeue());
+    }
+}
