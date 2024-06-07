7721aa53aa995ae5f2433271a7ee336d0bff3b17
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-08 01:08:09 +0530

    CircularQueue

--- Day_122_to_130/Leedcode/CircularQueue.java
+++ Day_122_to_130/Leedcode/CircularQueue.java
@@ -1,5 +1,8 @@
 package Leedcode;
 
+// question no 622
+// Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+question link: https://leetcode.com/problems/design-circular-queue/
 public class CircularQueue {
     
     private int[] items;
@@ -18,14 +21,43 @@ public class CircularQueue {
     }
 
     public boolean enQueue(int value) {
+
+        if( isFull() ){
+            return false;
+        }
+
+        items[rear] = value;
+        rear = (rear+1)%size;
+        count++;
+        return true;
         
     }
     public boolean deQueue() {
-        
+        if( isEmpty() ) return false;
+        front = (front+1)%size;
+        count--;
+        return true;
     }
 
     public int Front() {
-        
+        if( isEmpty() ) return -1;
+        return items[(front+1)%size];
+    }
+    
+    public int Rear() {
+        if( isEmpty() ) return -1;
+
+        int lastIndex = rear - 1;
+        if(lastIndex == -1) lastIndex = size - 1;
+        return items[lastIndex];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public boolean isFull() {
+        return count  == size;
     }
 
 }
