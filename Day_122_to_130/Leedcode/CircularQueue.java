a34d783d654782f158e727210d39d1ccade1a24e
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-08 00:58:43 +0530

    CircularQueue

---
+++ Day_122_to_130/Leedcode/CircularQueue.java
@@ -0,0 +1,31 @@
+package Leedcode;
+
+public class CircularQueue {
+    
+    private int[] items;
+    private int front;
+    private int rear;
+    private int size;
+    private int count;
+
+    public myCircularQueue(int k) {
+
+        this.items = new int[k];
+        this.front = -1;
+        this.rear = 0;
+        this.count = 0;
+        this.size = k;
+    }
+
+    public boolean enQueue(int value) {
+        
+    }
+    public boolean deQueue() {
+        
+    }
+
+    public int Front() {
+        
+    }
+
+}
