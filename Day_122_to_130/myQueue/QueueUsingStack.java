216cfdf2bb54f7aecbae5380a397344ebd250170
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-07 10:04:43 +0530

    testQueueUsingStack

--- Day_122_to_130/myQueue/QueueUsingStack.java
+++ Day_122_to_130/myQueue/QueueUsingStack.java
@@ -10,5 +10,24 @@ public class QueueUsingStack {
         s1 = new Stack<>();
         s2 = new Stack<>();
     }
-    
+
+    public void enqueue(int val){
+        s1.push(val);
+    }
+
+    public int dequeue(){
+        if(s1.isEmpty() && s2.isEmpty()){
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        if(s2.isEmpty()){
+            // copy all the items from s1 to s2
+
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
 }
