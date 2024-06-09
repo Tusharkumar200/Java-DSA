02fb4946fafbd552ce149fdaf56384893771c937
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-09 23:48:32 +0530

    init constructor

--- Day_122_to_130/Leedcode/Design_Double_Ended_queue.java
+++ Day_122_to_130/Leedcode/Design_Double_Ended_queue.java
@@ -1,5 +1,18 @@
 package Leedcode;
 
 public class Design_Double_Ended_queue{
-    
+    private int[] items;
+    private int front;
+    private int rear;
+    private int size;
+    private int count;
+
+    public Design_Double_Ended_queue(int n) {
+
+        items = new int[n];
+        front = 0;
+        rear = 0;
+        
+    }
+
 }
