cf8b00e247b3237d15c758db8eaf747869256528
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-11 23:44:14 +0530

    reverseStack

--- Day_122_to_130/Leedcode/Reverse_Queue_Recursion.java
+++ Day_122_to_130/Leedcode/Reverse_Queue_Recursion.java
@@ -1,5 +1,29 @@
 package Leedcode;
 
+import java.util.Stack;
+
 public class Reverse_Queue_Recursion {
-    
+    public static Stack<Integer> pushAtBottom(Stack <Integer> myStack,int x){
+        if(myStack.isEmpty()){
+            myStack.push(x);
+            return myStack;
+        }
+
+        int num = myStack.pop();
+        myStack = pushAtBottom(myStack,x);
+        myStack.push(num);
+
+        return myStack;
+        
+    }
+
+    public static void reverseStack(Stack <Integer> Stack){
+        if(Stack.isEmpty()){
+            return;
+        }
+
+        int num = Stack.pop();
+        reverseStack(Stack);
+        Stack = pushAtBottom(Stack,num);
+    }
 }
