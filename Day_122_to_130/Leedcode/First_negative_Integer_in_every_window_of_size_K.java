eff7cd604ba4865a41d98cb8b6cd67e56a922a9b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-12 17:19:37 +0530

    First_negative_Integer_in_every_window_of_size_K

--- Day_122_to_130/Leedcode/First_negative_Integer_in_every_window_of_size_K.java
+++ Day_122_to_130/Leedcode/First_negative_Integer_in_every_window_of_size_K.java
@@ -4,12 +4,12 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class First_negative_Integer_in_every_window_of_size_K {
-    public long[] printFirstNegativeInteger(long A[] , int N, int K){
+    public long[] printFirstNegativeInteger(long A[] , int N, int k){
 
         ArrayDeque <Integer> dq = new ArrayDeque<>();
         ArrayList  <Long> result = new ArrayList<>();
 
-        for(int i = 0; i < K; i++){
+        for(int i = 0; i < k; i++){
             if(A[i] < 0){
                 dq.add(i);
             }
@@ -19,5 +19,24 @@ public class First_negative_Integer_in_every_window_of_size_K {
         }else{
             result.add(0l);
         }
+
+        for(int i =k; i < N -1; i++){
+            if(!dq.isEmpty() && dq.getFirst() < i - k + 1){
+                dq.removeFirst();
+            }
+            if(A[i] < 0){
+                dq.add(i);
+            }
+            if(!dq.isEmpty()){
+                result.add(A[dq.getFirst()]);
+            }else{
+                result.add(0l);
+            }
+        }
+        long[] ans = new long[result.size()];
+        for(int i = 0; i < result.size(); i++){
+            ans[i] = result.get(i);
+        }
+        return ans;
     }
 }
