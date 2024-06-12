65dc51296d3e96474dbd76acd76cfbec65e1541d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-12 17:50:36 +0530

    Interleave_The_First_Half_Of_The_Queue_With_The_Second_Half

--- Day_122_to_130/Leedcode/Interleave_The_First_Half_Of_The_Queue_With_The_Second_Half.java
+++ Day_122_to_130/Leedcode/Interleave_The_First_Half_Of_The_Queue_With_The_Second_Half.java
@@ -1,5 +1,21 @@
 package Leedcode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Interleave_The_First_Half_Of_The_Queue_With_The_Second_Half {
     
+    public static void interLeaveQueue(Queue<Integer> q){
+        int size = q.size();
+        Queue<Integer> newQueue  = new ArrayDeque<>();
+
+        for(int i = 0; i < size/2; i++){
+            newQueue.offer(q.poll());
+        }
+
+        while(!newQueue.isEmpty()){
+            q.offer(newQueue.poll());
+            q.offer(q.poll());
+        }
+    }
 }
