a87d5c306f5afd41a5d2ea8e2c6e85b9a2bf3418
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-12 17:38:05 +0530

    Reverse_First_K_elements_of_Queue

--- Day_122_to_130/Leedcode/Reverse_First_K_elements_of_Queue.java
+++ Day_122_to_130/Leedcode/Reverse_First_K_elements_of_Queue.java
@@ -1,5 +1,25 @@
 package Leedcode;
 
+import java.util.Queue;
+
 public class Reverse_First_K_elements_of_Queue {
-    
+public Queue<Integer> reverseKElements(Queue <Integer> q, int k){
+
+    if(q.isEmpty() || k <= 0) return q;
+
+    int front = q.poll();
+    q = reverseKElements(q,k-1);
+    q.offer(front);
+
+    return q;
+}
+public Queue <Integer> modifyQueue(Queue <Integer> q, int k){
+
+    q = reverseKElements(q,k);
+
+    for(int i = 0; i < q.size()-k; i++){
+        q.offer(q.poll());
+    }
+    return q;
+}
 }
