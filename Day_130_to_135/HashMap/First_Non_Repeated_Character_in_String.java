89deedecb009e2520eb46491f2eb3fe7da80a73d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-13 21:55:42 +0530

    First_Non_Repeated_Character_in_String

--- Day_130_to_135/HashMap/First_Non_Repeated_Character_in_String.java
+++ Day_130_to_135/HashMap/First_Non_Repeated_Character_in_String.java
@@ -1,3 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class First_Non_Repeated_Character_in_String {
-    
+    public static void main(String[] args) {
+        Map<Character,Integer> freqMap = new HashMap<>();
+        String str = "a green apple";
+        
+        for(char ch:str.toCharArray()){
+            if(freqMap.containsKey(ch)){
+                freqMap.put(ch,freqMap.get(ch)+1);
+            }
+            else{
+                freqMap.put(ch,1);
+            }
+        }
+
+        for(char ch: str.toCharArray()){
+            if(freqMap.get(ch)==1){
+                System.out.println(ch);
+                break;
+            }
+        }
+        System.out.println(freqMap);
+    }
 }
