5de25f8951c5e1b036d323f7e51f9b4629b04eb7
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-14 23:36:09 +0530

    Entry

--- Day_130_to_135/MyHashMap/myHashMap.java
+++ Day_130_to_135/MyHashMap/myHashMap.java
@@ -1,5 +1,15 @@
 package MyHashMap;
 
+class Entry{
+    public int key;
+    public String value;
+
+     Entry(int key, String value){
+        this.key = key;
+        this.value = value;
+    }
+}
+
 public class myHashMap {
     
 }
