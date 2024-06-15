1f54a7d2eaf0b847b7dbcfb920b51f8c75ec5827
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-16 01:54:29 +0530

    Fibonacci_Series

--- Day_133_to_140/Recursion/Fibonacci_Series.java
+++ Day_133_to_140/Recursion/Fibonacci_Series.java
@@ -1,3 +1,16 @@
+
+
 public class Fibonacci_Series {
-    
+    private static int fib(int num){
+        if(num <= 1) return num;
+        return fib(num-2) + fib(num-1);
+    }
+
+    public static void main(String[] args) {
+        int num =8;
+        
+        for(int i =0; i<num;i++){
+            System.out.print(fib(i) + " ");
+        }
+    }
 }
