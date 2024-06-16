f7934ceeed1c8b90419dce39ba55ebf590c11abc
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-17 02:04:54 +0530

    _70_LC_ClimbingStairs

---
+++ Day_133_to_140/Recursion/_70_LC_ClimbingStairs.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class _70_LC_ClimbingStairs {
+    private static Map<Integer, Integer> map = new HashMap<>();
+
+    public static int climbStairs(int n){
+        if(map.containsKey(n)) return map.get(n);
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        int res = climbStairs(n-1) + climbStairs(n-2);
+        map.put(n, res);
+        return res;
+    }
+
+    // Testing ...
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(climbStairs(n));
+    }
+}
