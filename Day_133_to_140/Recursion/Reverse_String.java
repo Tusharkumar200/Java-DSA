7a411f4e64e738f2b8585a3461c91bd0903fd425
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-18 00:29:15 +0530

    reverseString

--- Day_133_to_140/Recursion/Reverse_String.java
+++ Day_133_to_140/Recursion/Reverse_String.java
@@ -1,5 +1,13 @@
 public class Reverse_String {
+    public static String reverseString(String str , int index){
+        if(index >= str.length()) return " ";
+        char ch = str.charAt(index);
+        String reversed = reverseString(str, index+1);
+        return reversed + ch;
+
+            }
     public static void main(String[] args) {
-        
+        System.out.println( reverseString("tushar",0));
+
     }
 }
