6892f7c08f3f87126127930fdee2f099dc248358
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-18 00:14:35 +0530

    sayDigits

--- Day_133_to_140/Recursion/Say_Digits.java
+++ Day_133_to_140/Recursion/Say_Digits.java
@@ -1,3 +1,16 @@
 public class Say_Digits {
-    
+
+    private static String[] digits = new String[]{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    public static void sayDigits(int num){
+        
+        if(num == 0) return;
+        
+        int lastDight = num%10;
+        sayDigits(num/10);
+        System.out.print(digits[lastDight] + " ");
+    }
+
+    public static void main(String[] args) {
+        sayDigits(123);
+    }
 }
