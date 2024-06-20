0c1905129db8ccc2d196b689ef6889a9e870a763
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-20 15:01:01 +0530

    _46_Permutations_of_String

--- Day_133_to_140/Recursion/LeetCode/_46_Permutations_of_String.java
+++ Day_133_to_140/Recursion/LeetCode/_46_Permutations_of_String.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class _46_Permutations_of_String{
-    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void solve(int[] nums, int index, List<List<Integer>> result){
+        if(index >= nums.length){
+            List<Integer> copy = new ArrayList<>();
+            for(int num : nums){
+                copy.add(num);
+            }
+            result.add(copy);
+            return;
+        }
+
+        for(int i = index; i < nums.length; i++){
+            swap(nums, i, index);
+            solve(nums, index + 1, result);
+            swap(nums, i, index);
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        solve(nums, 0, result);
+        return result;
+    }
 }
