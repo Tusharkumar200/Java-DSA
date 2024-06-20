bdc3fc69a918122fb5c19445135ce8eeafb0c444
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-21 00:31:06 +0530

    bubble_sort

---
+++ Day_133_to_140/sorting_algorithem/Main.java
@@ -0,0 +1,5 @@
+package sorting_algorithem;
+
+public class Main {
+    
+}
---
+++ Day_133_to_140/sorting_algorithem/bubble_sort.java
@@ -0,0 +1,7 @@
+public class bubble_sort {
+    private static void swap(int[] arr, int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
