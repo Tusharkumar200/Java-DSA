015c75033d611330930c9c537e5fde0abab29b1f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-22 02:17:59 +0530

    TestingDone

--- Day_133_to_140/sorting_algorithem/Main_bubble.java
+++ Day_133_to_140/sorting_algorithem/Main_bubble.java
@@ -5,7 +5,8 @@ public class Main_bubble {
         
         int[]arr ={10,5,2,-1,3,8,2,4,11,6};
         // sorting.bubblesort(arr);
-        sorting.selectionsort(arr);
+        // sorting.selectionsort(arr);
+        sorting.insertionSort(arr);
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
--- Day_133_to_140/sorting_algorithem/sorting.java
+++ Day_133_to_140/sorting_algorithem/sorting.java
@@ -31,6 +31,16 @@ public class sorting {
     }
 
     public static void insertionSort(int[] arr){
-        
+
+        for(int i=1; i<arr.length;i++){
+            int currentElement = arr[i];
+            int j = i-1;
+
+            while(j >= 0 && currentElement < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = currentElement;
+        }
     }
 }
