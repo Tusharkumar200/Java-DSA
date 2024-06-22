57f87a746bf83735800c50c6dafb25bdbb75922b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-23 00:12:44 +0530

    testing done

--- Day_133_to_140/sorting_algorithem/Main_bubble.java
+++ Day_133_to_140/sorting_algorithem/Main_bubble.java
@@ -3,10 +3,11 @@ package sorting_algorithem;
 public class Main_bubble {
     public static void main(String[] args) {
         
-        int[]arr ={10,5,2,-1,3,8,2,4,11,6};
+        int[]arr ={10,5,2,-1,3};
         // sorting.bubblesort(arr);
         // sorting.selectionsort(arr);
-        sorting.insertionSort(arr);
+        // sorting.insertionSort(arr);
+        sorting.mergeSort(arr);
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
