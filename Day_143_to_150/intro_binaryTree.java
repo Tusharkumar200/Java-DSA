b396d677c3e2e38a8aebc36ac9bb1847cb159b76
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-27 01:18:48 +0530

    intro_binaryTree

--- Day_143_to_150/intro_binaryTree.java
+++ Day_143_to_150/intro_binaryTree.java
@@ -3,10 +3,23 @@ class TreeNode{
     int data;
     TreeNode left;
     TreeNode right;
+
+    public TreeNode(int data){
+        this.data = data;
+    }
+
+    public String toString(){
+        return "value = "+ data;
+    }
 }
 
 public class intro_binaryTree{
     public static void main(String[] args) {
-        
+        var root = new TreeNode(7);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(8);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(10);
+
     }
 }
