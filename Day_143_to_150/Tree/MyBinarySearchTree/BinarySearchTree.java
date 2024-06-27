7635440920b86ce02c8e964acf131c5154ef9686
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-28 00:58:13 +0530

    insertfunction created

--- Day_143_to_150/Tree/MyBinarySearchTree/BinarySearchTree.java
+++ Day_143_to_150/Tree/MyBinarySearchTree/BinarySearchTree.java
@@ -1,5 +1,32 @@
 package Tree.MyBinarySearchTree;
 
+
+class TreeNode{
+    int data;
+    TreeNode leftChild;
+    TreeNode rightChild;
+
+    public TreeNode(int data){
+        this.data = data;
+    }
+
+    public String toString(){
+        return "value = "+ this.data;
+    }
+}
 public class BinarySearchTree {
-    
+    private TreeNode root;
+
+    public BinarySearchTree(){
+        this.root = null;
+    }
+
+    public void insert(int value){
+        TreeNode node = new TreeNode(value);
+
+        if(root == null){
+            root = node;
+            return;
+        }
+    }
 }
--- Day_143_to_150/Tree/MyBinarySearchTree/Main.java
+++ Day_143_to_150/Tree/MyBinarySearchTree/Main.java
@@ -1,5 +1,8 @@
 package Tree.MyBinarySearchTree;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(7);
+    }
 }
