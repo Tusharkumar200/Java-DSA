a85235560409671910195fea66fc861923493b9d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-06-28 23:23:15 +0530

    Travelling Nodes using Recursion

--- Day_143_to_150/Tree/MyBinarySearchTree/BinarySearchTree.java
+++ Day_143_to_150/Tree/MyBinarySearchTree/BinarySearchTree.java
@@ -21,29 +21,52 @@ public class BinarySearchTree {
         this.root = null;
     }
 
-    public void insert(int value){
-        TreeNode node = new TreeNode(value);
-
+    public TreeNode insert(TreeNode root ,int value){
         if(root == null){
-            root = node;
-            return;
+            System.out.println("Root is Empty,insert "+ value);
+            root = new TreeNode(value);
+            return root;
         }
-            TreeNode current = root;
-        while(true){
-            if(value <= current.data){ // Go Left
-                if(current.leftChild == null){
-                    current.leftChild = new TreeNode(value);
-                    break;
-                }
-                current = current.leftChild;
-            }
-            else{  // Go Right
-                if(current.rightChild == null){
-                    current.rightChild = new TreeNode(value);
-                    break;
-                }
-                current = current.rightChild;
-            }
+
+        if(value <= root.data){
+            System.out.println("Value "+ value + "is less the root");
+            System.out.println("Going to left");
+            root.leftChild = insert(root.leftChild, value);
         }
+        else{
+            System.out.println("Value "+ value + "is grater the root");
+            System.out.println("Going to right");
+            root.rightChild = insert(root.rightChild, value);
+        }
+
+       return root;
+    }
+
+    public void insert(int value){
+
+       root = insert(root,value);
+        // TreeNode node = new TreeNode(value);
+
+        // if(root == null){
+        //     root = node;
+        //     return;
+        // }
+        //     TreeNode current = root;
+        // while(true){
+        //     if(value <= current.data){ // Go Left
+        //         if(current.leftChild == null){
+        //             current.leftChild = new TreeNode(value);
+        //             break;
+        //         }
+        //         current = current.leftChild;
+        //     }
+        //     else{  // Go Right
+        //         if(current.rightChild == null){
+        //             current.rightChild = new TreeNode(value);
+        //             break;
+        //         }
+        //         current = current.rightChild;
+        //     }
+        // }
     }
 }
