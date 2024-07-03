1522181f112ae663a1b20b79b5cdef1ebe060a02
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-04 00:32:12 +0530

    validateBST

---
+++ Day_143_to_150/Tree/MyBinarySearchTree/LD_98_validate_BST.java
@@ -0,0 +1,10 @@
+package Tree.MyBinarySearchTree;
+
+public class LD_98_validate_BST {
+    private boolean validateBST(TreeNode root , int min , int max){
+        
+    }
+    public boolean isValidBST(TreeNode root){
+
+    }
+}
