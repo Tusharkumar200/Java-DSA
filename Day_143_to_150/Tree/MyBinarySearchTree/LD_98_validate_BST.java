dcca3574671969e674c39cc29ca68d78df16d413
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-04 00:43:35 +0530

    LD_98_validate_BST

--- Day_143_to_150/Tree/MyBinarySearchTree/LD_98_validate_BST.java
+++ Day_143_to_150/Tree/MyBinarySearchTree/LD_98_validate_BST.java
@@ -1,10 +1,30 @@
 package Tree.MyBinarySearchTree;
 
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val){
+        this.val = val;
+    }
+
+    public String toString(){
+        return "value = "+ this.val;
+    }
+}
+
 public class LD_98_validate_BST {
-    private boolean validateBST(TreeNode root , int min , int max){
-        
+
+        private boolean validateBST(TreeNode root , long min , long max){
+        if(root == null) return true;
+
+        if(root.val <=min || root.val >= max) return false;
+
+        return validateBST(root.left , min , root.val) && 
+                validateBST(root.right , root.val , max);
     }
     public boolean isValidBST(TreeNode root){
-
+        return validateBST(root,Long.MIN_VALUE , Long.MAX_VALUE);
     }
 }
