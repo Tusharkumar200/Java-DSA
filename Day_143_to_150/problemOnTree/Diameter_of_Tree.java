be3f068b1f7e110f33e948fd6dbed44154580c80
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-10 21:54:01 +0530

    Diameter_of_Tree

---
+++ Day_143_to_150/problemOnTree/Diameter_of_Tree.java
@@ -0,0 +1,16 @@
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Diameter_of_Tree {
+    
+}
