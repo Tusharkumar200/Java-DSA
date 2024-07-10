ca94a4b12fe47a097ca1ea110ddfe090980ab350
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-10 22:32:29 +0530

    tested diameterFast

--- Day_143_to_150/problemOnTree/Pair.java
+++ Day_143_to_150/problemOnTree/Pair.java
@@ -53,12 +53,14 @@ public class Diameter_of_Tree {
     }
     
     int diameter(Node root){
-        if(root == null) return 0;
+        // if(root == null) return 0;
 
-        int leftDia = diameter(root.left);
-        int rightDia = diameter(root.right);
-        int myDia = getHeight(root.left) + getHeight(root.right) + 1;
+        // int leftDia = diameter(root.left);
+        // int rightDia = diameter(root.right);
+        // int myDia = getHeight(root.left) + getHeight(root.right) + 1;
 
-        return Math.max(myDia, Math.max(leftDia, rightDia));
+        // return Math.max(myDia, Math.max(leftDia, rightDia));
+
+        return diameterFast(root).dia;
     }
 }
