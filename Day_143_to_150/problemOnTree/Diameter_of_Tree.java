c1466695e6c72a024558ea3656897fd5932b83a5
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-10 22:02:35 +0530

    getHeight

--- Day_143_to_150/problemOnTree/Diameter_of_Tree.java
+++ Day_143_to_150/problemOnTree/Diameter_of_Tree.java
@@ -12,6 +12,16 @@ class Node{
 }
 
 public class Diameter_of_Tree {
+
+    private int getHeight(Node root){
+        if(root == null) return 0;
+
+        int left = getHeight(root.left);
+        int right  = getHeight(root.right);
+
+        return Math.max(left, right) + 1;
+
+    }
     
     int diameter(Node root){
         if(root == null) return 0;
