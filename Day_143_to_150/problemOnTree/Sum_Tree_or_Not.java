c69fe2d084b12f3ece14aa85a12de0a227901d86
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-12 21:09:25 +0530

    Sum_Tree_or_Not

---
+++ Day_143_to_150/problemOnTree/Sum_Tree_or_Not.java
@@ -0,0 +1,16 @@
+
+public class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Sum_Tree_or_Not {
+    
+}
