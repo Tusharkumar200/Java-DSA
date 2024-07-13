4cb06f6df957ebf5c9d2eb36fdf87c862e6b4c5c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-13 21:39:04 +0530

    Lc_103_Zig_Zag_Traversal

--- Day_143_to_150/problemOnTree/Lc_103_Zig_Zag_Traversal.java
+++ Day_143_to_150/problemOnTree/Lc_103_Zig_Zag_Traversal.java
@@ -1,4 +1,6 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class TreeNode{
     int val;
@@ -16,6 +18,34 @@ class TreeNode{
 
 public class Lc_103_Zig_Zag_Traversal {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root){
-        
+        List <List<Integer>> result = new LinkedList<>();
+        if(root == null) return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        boolean isLeft = true;
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            List<Integer> temp = new LinkedList<>();
+            int size = q.size();
+
+            for(int i=0 ; i<size; i++){
+                TreeNode currentNode = q.poll();
+
+                if(isLeft) temp.add(currentNode.val);
+                else temp.add(0, currentNode.val);
+
+                if(currentNode.left != null){
+                    q.offer(currentNode.left);
+                }
+
+                if(currentNode.right != null){
+                    q.offer(currentNode.right);
+                }
+            }
+            result.add(temp);
+            isLeft = !isLeft;
+        }
+        return result;
     }
 }
