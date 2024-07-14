b87ab760754354119da04fb74a258735a978b8af
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-14 06:38:12 +0530

    _GFG_boundary_traversal

---
+++ Day_143_to_150/problemOnTree/_GFG_boundary_traversal.java
@@ -0,0 +1,15 @@
+public class _GFG_boundary_traversal {
+    
+}
+
+public class Node{
+    
+    int data;
+    Node left, right;
+
+    Node(int data){
+         this.data =data;
+        left = right = null;
+    }
+
+}
