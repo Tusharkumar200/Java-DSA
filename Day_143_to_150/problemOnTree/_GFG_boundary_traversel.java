e5304d662dd372ec9f5704d43a1eed6c0cf48302
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-14 07:15:07 +0530

    boundary

--- Day_143_to_150/problemOnTree/_GFG_boundary_traversel.java
+++ Day_143_to_150/problemOnTree/_GFG_boundary_traversel.java
@@ -4,31 +4,63 @@ public class _GFG_boundary_traversel {
     public static void main(String[] args) {
         
     }
-    void traverselLeft(Node root , ArrayList<Integer> result){
+    void traverseLeaf(Node root , ArrayList<Integer> result){
         if(root == null) return;
 
         if(root.left == null && root.right == null){
             result.add(root.data);
             return;
         }
-        traverselLeft(root.left, result);
-        traverselLeft(root.right, result);
+        traverseLeaf(root.left, result);
+        traverseLeaf(root.right, result);
       }
 
       void traverseLeft(Node root , ArrayList<Integer> result){
         if(root == null) return;
         if(root.left == null && root.right == null) return;
         
-        
+        result.add(root.data);
+
+        if(root.left != null){
+            traverseLeft(root.left, result);
+
         }
+        else{
+            traverseLeft(root.right, result);
+        }
+
       }
+      
+      void traverseRightRevOrder(Node root, ArrayList<Integer> result){
+            if(root == null) return;
+            if(root.left == null && root.right == null) return;
+
+            if(root.right != null){
+                traverseRightRevOrder(root.right, result);
     
+            }
+            else{
+                traverseRightRevOrder(root.right, result);
+            }
+            result.add(root.data);
+
+      }
       ArrayList <Integer> boundary(Node node){
+        ArrayList<Integer> result = new ArrayList<>();
+        if(node == null) return result;
+
+        result.add(node.data); //Root
+        traverseLeft(node.left, result); //Left
+        traverseLeaf(node.left, result); //Leaf left
+        traverseLeaf(node.right, result); //right left
+
+        traverseRightRevOrder(node.right, result); //Right rev
+        return result;
 
       }
 }
 
-public class Node{
+ class Node{
     
     int data;
     Node left, right;
