88fae2ec4a9138c30ee12b44006de3dcdc05a654
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-15 17:43:37 +0530

    vertical_order_traversal

--- Day_143_to_150/problemOnTree/vertical_order_traversal.java
+++ Day_143_to_150/problemOnTree/vertical_order_traversal.java
@@ -1,5 +1,14 @@
 
+class Node{
+    int data;
+    Node left;
+    Node right;
 
+    public Node(int data){
+        this.data  = data;
+        left = right = null;
+    }
+}
 
 public class vertical_order_traversal {
     
