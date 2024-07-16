dfa96f2c5f87a8066181db3c7de8e9dc56fc6bf2
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-16 21:12:07 +0530

    _GFG_topView

---
+++ Day_143_to_150/problemOnTree/_GFG_topView.java
@@ -0,0 +1,17 @@
+
+
+class Node{
+    
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        left = right = null;
+    
+    }
+}
+public class _GFG_topView{
+    
+}
