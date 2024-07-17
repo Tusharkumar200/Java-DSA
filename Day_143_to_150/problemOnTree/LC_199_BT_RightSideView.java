bea2b4f7fb5f62d67dea1d70cc5a639dea4d5d34
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-17 22:01:29 +0530

    Lc_199_BT_RightSideView

--- Day_143_to_150/problemOnTree/LC_199_BT_RightSideView.java
+++ Day_143_to_150/problemOnTree/LC_199_BT_RightSideView.java
@@ -1,4 +1,22 @@
 
-public class Lc_199_BT_RightSideView{
-    
+public class TreeNode{
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+
+
+}
+
+ class Lc_199_BT_RightSideView{
+
+    public List<Integer> rightSideView(TreeNode root) {
+        
+    }
 }
