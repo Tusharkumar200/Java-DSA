0f5411396f5608be7ac9eec8e53c9aea097be727
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-18 23:29:43 +0530

    Diagonal_Traversal

--- Day_143_to_150/problemOnTree/Diagonal_Traversal.java
+++ Day_143_to_150/problemOnTree/Diagonal_Traversal.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Node{
     
     int data;
@@ -12,5 +16,26 @@ class Node{
 }
 
 public class Diagonal_Traversal {
-    
+    public ArrayList <Integer> diagonal(Node root)
+    {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(root == null) return result;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        
+        while (q.isEmpty()) {
+            Node current = q.poll();
+
+            while(current != null){
+                result.add(current.data);
+
+                if(current.left != null){
+                    q.offer(current.left);
+                }
+                current = current.right;
+            }
+        }
+        return result;
+    }
 }
