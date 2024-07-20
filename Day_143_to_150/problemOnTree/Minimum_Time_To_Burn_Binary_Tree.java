5a871ab5d97515c8e7760920220fefb572b1f785
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-20 10:52:37 +0530

    Minimum_Time_To_Burn_Binary_Tree

--- Day_143_to_150/problemOnTree/Minimum_Time_To_Burn_Binary_Tree.java
+++ Day_143_to_150/problemOnTree/Minimum_Time_To_Burn_Binary_Tree.java
@@ -1,3 +1,19 @@
-public class Minimum_Time_To_Burn_Binary_Tree {
+
+class Node{
+    int data;
+    Node left;
+    Node right;
     
+    public Node(int data){
+        this.data =data;
+        left = right = null;
+    }
+}
+
+public class Minimum_Time_To_Burn_Binary_Tree {
+
+    
+    public static int minTime(Node root , int target){
+
+    }
 }
