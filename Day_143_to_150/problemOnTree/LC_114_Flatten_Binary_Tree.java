d9be687d76f3673471b2f47105ae6287a557091f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-21 23:15:53 +0530

    LC_114_Flatten_Binary_Tree

---
+++ Day_143_to_150/problemOnTree/LC_114_Flatten_Binary_Tree.java
@@ -0,0 +1,21 @@
+
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(){}
+    public TreeNode(int val){this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+
+}
+
+public class LC_114_Flatten_Binary_Tree {
+    
+}
