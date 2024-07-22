a5e9be2c951ddded1b2fa9be4d7b99c148476239
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-22 21:57:38 +0530

    TreeNode created

--- Day_143_to_150/problemOnTree/Inorder_Predecessor_Successor_in_BTS.java
+++ Day_143_to_150/problemOnTree/Inorder_Predecessor_Successor_in_BTS.java
@@ -1,5 +1,15 @@
 
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
 
+    TreeNode(int data){
+        this.data = data;
+        left = right = null;
+    }
+
+}
 public class Inorder_Predecessor_Successor_in_BTS {
     
 }
