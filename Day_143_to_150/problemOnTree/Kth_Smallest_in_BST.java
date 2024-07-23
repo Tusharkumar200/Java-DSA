26f1d16805a4e522a4ffdb2efb66c3f77be3ed8b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-23 21:24:40 +0530

    Kth_Smallest_in_BST

--- Day_143_to_150/problemOnTree/Kth_Smallest_in_BST.java
+++ Day_143_to_150/problemOnTree/Kth_Smallest_in_BST.java
@@ -1,1 +1,16 @@
-// leetcode 2
+// leetcode 230  question number
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Kth_Smallest_in_BST {
+    public int kthSmallest(TreeNode root, int k) {
+        
+    }
+    
+    
+}
