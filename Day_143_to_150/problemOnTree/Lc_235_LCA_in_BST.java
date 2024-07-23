16d8aeaabb7cccf0cef78f777e74489f86f6e0df
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-23 22:04:50 +0530

    Lc_235_LCA_in_BST

--- Day_143_to_150/problemOnTree/Lc_235_LCA_in_BST.java
+++ Day_143_to_150/problemOnTree/Lc_235_LCA_in_BST.java
@@ -7,5 +7,18 @@ class TreeNode {
 }
 
 public class Lc_235_LCA_in_BST {
-    
+    public TreeNode lowestCommonAncestor(TreeNode root , TreeNode p ,TreeNode q){
+        if(root == null) return null;
+         if(root == p || root == q) return root;
+
+         if(p.val < root.val && q.val < root.val){
+             return lowestCommonAncestor(root.left, p, q);
+         }
+
+         if(p.val > root.val && q.val > root.val){
+             return lowestCommonAncestor(root.right, p, q);
+         }
+
+         return root;
+    }
 }
