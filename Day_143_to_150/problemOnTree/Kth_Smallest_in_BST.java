65d6648a86b62f8fb71473f23088790bb7a1a88c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-23 21:35:18 +0530

    inOrder_Method

--- Day_143_to_150/problemOnTree/Kth_Smallest_in_BST.java
+++ Day_143_to_150/problemOnTree/Kth_Smallest_in_BST.java
@@ -1,4 +1,8 @@
 // leetcode 230  question number
+// Difficulty: Medium
+// Problem Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+import java.util.ArrayList;
+import java.util.List;
 
 class TreeNode {
     int val;
@@ -8,8 +12,19 @@ class TreeNode {
 }
 
 public class Kth_Smallest_in_BST {
+
+    private void inorder(TreeNode root, List<Integer> result){
+        if(root != null){
+            inorder(root.left, result);
+            result.add(root.val);
+            inorder(root.right, result);
+        }
+    }
+
     public int kthSmallest(TreeNode root, int k) {
-        
+        ArrayList<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result.get(k-1);
     }
     
     
