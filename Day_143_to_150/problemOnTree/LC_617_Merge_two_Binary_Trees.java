4aca65bf60a5a1faff347df94c517c2f0168183d
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-23 22:17:33 +0530

    LC_617_Merge_two_Binary_Trees

--- Day_143_to_150/problemOnTree/LC_617_Merge_two_Binary_Trees.java
+++ Day_143_to_150/problemOnTree/LC_617_Merge_two_Binary_Trees.java
@@ -10,4 +10,18 @@ class TreeNode {
 
 public class LC_617_Merge_two_Binary_Trees {
     
+    public TreeNode mergeTrees(TreeNode root1 , TreeNode root2){
+        if(root1 == null) return root2;
+        if(root2 == null) return root1;
+
+        TreeNode node = new TreeNode(root1.val + root2.val);
+
+        TreeNode left = mergeTrees(root1.left, root2.left);
+        TreeNode right = mergeTrees(root1.right, root2.right);
+
+        node.left = left;
+        node.right = right;
+
+        return node;
+    }
 }
