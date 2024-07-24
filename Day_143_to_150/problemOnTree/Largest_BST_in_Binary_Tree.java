98e0d69a68ba87463e7885c9db2a6020e9e59d21
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-24 22:46:54 +0530

    Largest_BST_in_Binary_Tree

--- Day_143_to_150/problemOnTree/Largest_BST_in_Binary_Tree.java
+++ Day_143_to_150/problemOnTree/Largest_BST_in_Binary_Tree.java
@@ -1,3 +1,16 @@
+
+class TreeNode<T>{
+    public T data;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
+
+    TreeNode(T data){
+        this.data = data;
+        left = right = null;
+    }
+
+}
+
 public class Largest_BST_in_Binary_Tree {
     
 }
