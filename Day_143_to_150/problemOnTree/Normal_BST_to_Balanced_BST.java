312d74a7d03ad0b67bf9e994ef93c89102ae0c1f
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-24 20:48:43 +0530

    Normal_BST_to_Balanced_BST

---
+++ Day_143_to_150/problemOnTree/Normal_BST_to_Balanced_BST.java
@@ -0,0 +1,16 @@
+
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class Normal_BST_to_Balanced_BST {
+    
+}
