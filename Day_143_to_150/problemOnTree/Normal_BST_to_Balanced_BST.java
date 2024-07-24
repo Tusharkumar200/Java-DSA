20a2b4aee583cf6707c82537fa664228610adf2e
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-24 22:42:57 +0530

    buildBalancedTree

--- Day_143_to_150/problemOnTree/Normal_BST_to_Balanced_BST.java
+++ Day_143_to_150/problemOnTree/Normal_BST_to_Balanced_BST.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 class Node{
     int data;
@@ -12,5 +13,30 @@ class Node{
 }
 
 public class Normal_BST_to_Balanced_BST {
+
+    private void getInorder(Node root , List<Integer> list){
+            if(root == null) return;
+
+            getInorder(root.left , list);
+            list.add(root.data);
+            getInorder(root.right, list);
+    }
     
+    private Node solve(Node root, int start ,int end , List<Integer> inorder){
+        if(start > end ) return null;
+
+        int mid = (start + end) / 2;
+        int num = inorder.get(mid);
+
+        Node node = new Node(num);
+        node.left = solve(start , mid -1 , inorder);
+        node right = solve(mid + 1 , end ,inorder);
+        return node;
+    }
+    
+    Node buildBalancedTree(Node root){
+        ArrayList<Integer> list = new ArrayList<>();
+        getInorder(root, list);
+        return solve(0 , list.size() -1 , list);
+    }
 }
