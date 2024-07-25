126ad49a81a09ca805c38b7dff03e9988331d1d9
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-25 22:21:19 +0530

    BUG Fixed

--- Day_170_to_180/AVL_TREE/main.java
+++ Day_170_to_180/AVL_TREE/main.java
@@ -1,15 +1,15 @@
 
 package AVL_TREE;
 
-public class main {
-    public static void main(String args[]){
-        
-        AVLTREE tree = new AVLTREE();
-        int[] values = new int[]{7,5,8,2,4,6};
+public class Main {
+	public static void main(String args[]){
+		
+		AVLTREE tree = new AVLTREE();
+		int[] values = new int[]{7,5,8,2,4,6};
 
-        for(int val: values){
-            tree.insert(val);
-        }
-        System.out.println(tree);
-    }
+		for(int val: values){
+			tree.insert(val);
+		}
+		System.out.println(tree);
+	}
 }
