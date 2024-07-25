538d68e362f66c3a232d2dc80f14ec1fcfc26fc4
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-25 22:16:04 +0530

    AVLTREE

--- Day_170_to_180/AVL_TREE/main.java
+++ Day_170_to_180/AVL_TREE/main.java
@@ -1,9 +1,10 @@
 
 package AVL_TREE;
+
 public class main {
     public static void main(String args[]){
         
-        AVLTree tree = new AVLTree();
+        AVLTREE tree = new AVLTREE();
         int[] values = new int[]{7,5,8,2,4,6};
 
         for(int val: values){
