58696358926e31582f0233e2445bd064201af2cb
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-25 22:14:02 +0530

    insertion in avl

--- Day_170_to_180/AVL_TREE/main.java
+++ Day_170_to_180/AVL_TREE/main.java
@@ -1,3 +1,14 @@
+
+package AVL_TREE;
 public class main {
-    
+    public static void main(String args[]){
+        
+        AVLTree tree = new AVLTree();
+        int[] values = new int[]{7,5,8,2,4,6};
+
+        for(int val: values){
+            tree.insert(val);
+        }
+        System.out.println(tree);
+    }
 }
