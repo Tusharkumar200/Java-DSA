7d742fd7f8d94bffc2d20926a04d886776ad407c
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-26 22:29:03 +0530

    tested Imbalanced Node

--- Day_170_to_180/AVL_TREE/main.java
+++ Day_170_to_180/AVL_TREE/main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String args[]){
 		
 		AVLTREE tree = new AVLTREE();
-		int[] values = new int[]{7,5,8,2,4,6};
+		// int[] values = new int[]{7,5,8,2,4,6};
+		int[] values = new int[]{10,5,3};
 
 		for(int val: values){
 			tree.insert(val);
