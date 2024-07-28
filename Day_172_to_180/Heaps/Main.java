2b4945f25e18c0aa45878f913b2ee22d1e848fb5
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-28 10:25:27 +0530

    Testing Done

--- Day_172_to_180/Heaps/Main.java
+++ Day_172_to_180/Heaps/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = new int[]{3,10,2,4,16,8};
+        int[] nums = new int[]{3,10,2,4,16,8,90};
         MaxHeap maxHeap = new MaxHeap(nums.length);
 
         for(int num: nums){
