6e26ead44a6eaed2875eb2f195e019576bc5e043
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-28 10:18:30 +0530

    insertMethod

--- Day_172_to_180/Heaps/Main.java
+++ Day_172_to_180/Heaps/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int[] nums = new int[]{3,10,2,4,16,8};
+        MaxHeap maxHeap = new MaxHeap(nums.length);
+
+        for(int num: nums){
+            maxHeap.insert(num);
+        }
+        System.out.println("Done.");
     }
 }
--- Day_172_to_180/Heaps/MaxHeap.java
+++ Day_172_to_180/Heaps/MaxHeap.java
@@ -1,5 +1,39 @@
 
 
 public class MaxHeap{
-    
+    private int[] items;
+    private int size;
+
+    public MaxHeap(int size){
+        this.items = new int[size];
+        this.size = 0;
+    }
+
+    public void insert(int value){
+        // Insert element at available slot
+        items[size] = value;
+        size++;
+
+        // Bubble up
+        int index = size - 1;  
+        while(index >0 &&items[index] > getParent(index)){
+            swapElementWithIndex(index, getParentIndex(index));
+            index = getParentIndex(index);
+
+        }
+    }
+
+    private int getParentIndex(int index){
+        return (index - 1) / 2;
+    }
+
+    private int getParent(int index){
+        return items[getParentIndex(index)];
+    }
+
+    private void swapElementWithIndex(int idx1 , int idx2){
+        int temp = items[idx1];
+        items[idx1] = items[idx2];
+        items[idx2] = temp;
+    }
 }
