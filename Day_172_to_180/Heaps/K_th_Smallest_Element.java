32dcf49c3faadb6bff70b9a337364f29a667be68
Tushar Kumar <tusharnwd2003@gmail.com>
2024-07-30 18:24:16 +0530

    K_th_Smallest_Element

--- Day_172_to_180/Heaps/K_th_Smallest_Element.java
+++ Day_172_to_180/Heaps/K_th_Smallest_Element.java
@@ -1,5 +1,44 @@
 
 
 public class K_th_Smallest_Element {
-        
+
+    private static void heapify(int[] arr, int i){
+
+        int maxIndex = i;
+        int size = arr.length;
+        int lc= i * 2+1;
+        int rc= i * 2+2;
+
+        if(lc < size && arr[maxIndex] < arr[lc]) maxIndex = lc;
+        if(rc < size && arr[maxIndex] < arr[rc]) maxIndex = rc;
+
+        if(maxIndex == i) return ;
+
+        int t = arr[maxIndex];
+        arr[maxIndex] = arr[i];
+        arr[i] = t;
+        heapify(arr,maxIndex);
+    }
+        public static int kthSmallest(int[] arr, int l , int r, int k){
+            int [] heap = new int[arr.length];
+
+            // put K elements in the heap
+
+            for(int i=0; i<k; i++){
+                heap[i] = arr[i];
+              
+            }
+//          Normal Heapify
+            for(int i=k -1; i>=0; i--){
+                    heapify(heap,i);
+            }
+
+            for(int i = k; i<arr.length; i++){
+                if(arr[i] > heap[0]) continue;
+                heap[0] = arr[i];
+                heapify(heap, 0);
+            }
+
+            return heap[0];
+        }
 }
