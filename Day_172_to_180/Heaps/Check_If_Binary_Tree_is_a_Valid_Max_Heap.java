4ef3b0dd1c48b063b1935600cd4005ae4b2188a2
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-02 23:44:12 +0530

    Check_If_Binary_Tree_is_a_Valid_Max_Heap

--- Day_172_to_180/Heaps/Check_If_Binary_Tree_is_a_Valid_Max_Heap.java
+++ Day_172_to_180/Heaps/Check_If_Binary_Tree_is_a_Valid_Max_Heap.java
@@ -1,9 +1,18 @@
 
+ class Node{
+        int data;
+        Node left;
+        Node right;
 
+        Node(int d){
+            this.data = d;
+            left = right = null;
+        }
+ }
 
 public class Check_If_Binary_Tree_is_a_Valid_Max_Heap{
     //  preparing for exam!
     // exam  prepration
-    
+
 
 }
