7a9915d10eb69bc38fb62d409ac40185af383123
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-03 00:10:43 +0530

    Check_If_Binary_Tree_is_a_Valid_Max_Heap

--- Day_172_to_180/Heaps/Check_If_Binary_Tree_is_a_Valid_Max_Heap.java
+++ Day_172_to_180/Heaps/Check_If_Binary_Tree_is_a_Valid_Max_Heap.java
@@ -12,18 +12,40 @@
 
 public class Check_If_Binary_Tree_is_a_Valid_Max_Heap{
   
-    private int totalNodes(Node root){
+    private int getTotalNodes(Node root){
 
         if(root == null) return 0;
 
-        return 1 + totalNodes(root.left) + totalNodes(root.right);
+        return 1 + getTotalNodes(root.left) + getTotalNodes(root.right);
     }
+
+    private boolean isCBT(Node root , int index , int totalNodes){
+        if(root == null) return true;
+
+        if(index >= totalNodes) return false;
+
+        return isCBT(root.left , index * 2+1 , totalNodes) && isCBT(root.right , index * 2+2 , totalNodes);
+    }
+     private boolean isMaxTree(Node root){
+        if(root == null) return true;
+
+         if(root.left == null && root.right == null) return true;
+
+         if(root.right == null){
+            return root.data > root.left.data;
+         }
+
+         return root.data > root.left.data && 
+         root.data > root.right.data && isMaxTree(root.left) && isMaxTree(root.right);
+
+        
+     }
     boolean isHeap(Node tree){
-        if(tree == null) return null;
+        if(tree == null) return true;
 
-        int totalNodes = totalNodes(tree);
+        int totalNodes = getTotalNodes(tree);
 
-        return isCBT() && isMaxTree();
+        return isCBT(tree ,0, totalNodes) && isMaxTree(tree);
     }
 
 
