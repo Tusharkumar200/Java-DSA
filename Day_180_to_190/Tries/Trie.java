33453e035b84d4dc896628d935ce288c32886468
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-05 10:15:28 +0530

    insert method

--- Day_180_to_190/Tries/Trie.java
+++ Day_180_to_190/Tries/Trie.java
@@ -9,8 +9,31 @@ class TrieNode{
     this.children = new TrieNode[26];
     this.isEndOfWord = false;
     }
+
+    public String toString(){
+        return "Value = "+value;
+    }
 }
 
 public class Trie{
+    private TrieNode root;
 
+    public Trie(){
+        this.root = new TrieNode(' ')
+    }
+
+    public void insert(String word){
+        TrieNode current = root;
+
+        for(char ch: word.toCharArray()){
+
+            int index = ch - 'a';
+
+            if(current.children[index] == null){
+                current.children[index] = new TrieNode(ch);
+            }
+            current = current.children[index];
+        }
+        current.isEndOfWord = true;
+    }
 }
