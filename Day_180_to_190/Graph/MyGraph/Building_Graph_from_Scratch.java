ea4850d58d4ab50d9722fa993debbf0031d214eb
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-08 21:17:08 +0530

    creatNode

--- Day_180_to_190/Graph/MyGraph/Building_Graph_from_Scratch.java
+++ Day_180_to_190/Graph/MyGraph/Building_Graph_from_Scratch.java
@@ -1,5 +1,32 @@
 package Graph.MyGraph;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Building_Graph_from_Scratch {
+    class Node {
+        public String value;
     
+        public Node(String value) {
+            this.value = value;
+        }
+    }
+
+    private Map<Node , List<Node>> adjacencyList;
+
+    private Map<String, Node> nodes;
+
+    public Building_Graph_from_Scratch(){
+        this.adjacencyList = new HashMap<Node , List<Node>>();
+        this.nodes = new HashMap<>();
+    }
+    
+    public Node creatNode(String value){
+
+        if(!nodes.containsKey(value))
+            nodes.put(value, new Node(value));
+        
+        return nodes.get(value);
+    }
 }
