0a217e701fc08ddd49181f2abb18ae77a35d57cb
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-11 11:24:42 +0530

    Testing of DFS done

--- Day_180_to_190/Graph/MyGraph/Main.java
+++ Day_180_to_190/Graph/MyGraph/Main.java
@@ -8,14 +8,22 @@ public class Main {
         var b=  graph.createNode("B");
         var c=  graph.createNode("C");
         var d=  graph.createNode("D");
+        var e=  graph.createNode("E");
+        var f=  graph.createNode("F");
+        var g=  graph.createNode("G");
 
         graph.createConnection(a,b, GRAPH_DIRECTION.DIRECTED);
-        graph.createConnection(b,c, GRAPH_DIRECTION.DIRECTED);
-        graph.createConnection(c,a, GRAPH_DIRECTION.DIRECTED);
-        graph.createConnection(d,a, GRAPH_DIRECTION.DIRECTED);
+        graph.createConnection(a,c, GRAPH_DIRECTION.DIRECTED);
+        graph.createConnection(b,d, GRAPH_DIRECTION.DIRECTED);
+        graph.createConnection(b,e, GRAPH_DIRECTION.DIRECTED);
+        graph.createConnection(c,f, GRAPH_DIRECTION.DIRECTED);
+        graph.createConnection(e,f, GRAPH_DIRECTION.DIRECTED);
+        graph.createConnection(d,g, GRAPH_DIRECTION.DIRECTED);
+        graph.createConnection(f,g, GRAPH_DIRECTION.DIRECTED);
 
         System.out.println(graph);
         System.out.println("BFS" + graph.getBFS(a));
+        System.out.println("DFS" + graph.getDFS(a));
 
         
     }
