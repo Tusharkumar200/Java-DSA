f6031c3f5cf022f9dc581e4a98aa9c6fa823bd30
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-13 17:49:17 +0530

    GFG_Topologicle_Sort

---
+++ Day_180_to_190/Graph/MyGraph/GFG_Topologicle_Sort.java
@@ -0,0 +1,15 @@
+package Graph.MyGraph;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Stack;
+
+public class GFG_Topologicle_Sort {
+    static void solve(Integer node , Set<Integer> visited , Stack <Integer> stack){
+        
+    }
+
+    static int[] topoSort(int v, ArrayList<ArrayList<Integer>> adj) {
+        Set<Integer> visited = new HashSet<>();
+    }
+}
