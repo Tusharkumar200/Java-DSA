a31e0645ae9c3f883059fa4c8e4f26b81ecd45d2
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-16 15:31:54 +0530

    shortestPath

--- Day_180_to_190/Graph/MyGraph/GFG_Shortest_path_DAG.java
+++ Day_180_to_190/Graph/MyGraph/GFG_Shortest_path_DAG.java
@@ -1,7 +1,28 @@
 package Graph.MyGraph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GFG_Shortest_path_DAG {
+    private Map<Integer , List<int[]>> getAdjList(int[][] edges){
+        Map<Integer , List<int[]>> map = new HashMap<>();
+        for(var edge: edges){
+            int from  = edge[0];
+            int to = edge[1];
+            int dist = edge[2];
+
+            map.putIfAbsent(from , new ArrayList<>());
+            map.get(from).add(new int[]{to , distance });
+        }
+        return map;
+    }
+
+    
     public int[] shortestPath(int N,int M, int[][] edges) {
-		//Code here
+         Map<Integer , List<int[]>> adj = getAdjList(edges);
+
+		
 	}
 }
