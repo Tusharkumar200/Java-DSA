89474a31e8fa9f13f4b01a730baea3394b33925b
Tushar Kumar <tusharnwd2003@gmail.com>
2024-08-17 13:13:13 +0530

    WeightedEdge

--- Day_180_to_190/Graph/MyGraph/UndirectedWeightedGraph.java
+++ Day_180_to_190/Graph/MyGraph/UndirectedWeightedGraph.java
@@ -1,5 +1,20 @@
 
 
-public class UndirectedWeightedGraph{
+class Node{
+    public String label;
+
+    private List<WeightedEdge> edges;
+
+    public Node(String label){
+        this.label = label;
+        this.edges = new ArrayList<WeightedEdge>();
+    }
+}
+
+class WeightedEdge{
     
+}
+
+public class UndirectedWeightedGraph{
+
 }
